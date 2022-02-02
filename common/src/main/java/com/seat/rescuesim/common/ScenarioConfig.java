f99c236380fcbd06109ea6219940f27bc5ce348d
isr413 <isr413@utulsa.edu>
2022-02-02 10:41:18 -0600

    Adds drone and victim specs

--- common/src/main/java/com/seat/rescuesim/common/ScenarioConfig.java
+++ common/src/main/java/com/seat/rescuesim/common/ScenarioConfig.java
@@ -5,29 +5,33 @@ import org.json.JSONObject;
 
 public class ScenarioConfig {
     private static final String DISASTER_SCALE = "disaster_scale";
+    private static final String DRONE_SPEC = "drone_spec";
     private static final String MAP = "map";
     private static final String MISSION_LENGTH = "mission_length";
+    private static final String NUM_DRONES = "num_drones";
     private static final String NUM_VICTIMS = "num_victims";
     private static final String STEP_SIZE = "step_size";
-    private static final String VICTIM_MOVE_SPEED_MEAN = "victim_move_speed_mean";
-    private static final String VICTIM_MOVE_SPEED_STD_DEV = "victim_move_speed_std_dev";
+    private static final String VICTIM_SPEC = "victim_spec";
 
     private double disasterScaleParam;
+    private DroneSpecification droneSpec;
     private Map map;
     private int missionLength;
+    private int numDrones;
     private int numVictims;
     private double stepSize;
-    private Tuple<Double, Double> victimMoveSpeedDistParams;
+    private VictimSpecification victimSpec;
 
     public static ScenarioConfig decode(JSONObject json) throws JSONException {
+        int numDrones = json.getInt(ScenarioConfig.NUM_DRONES);
+        DroneSpecification droneSpec = DroneSpecification.decode(json.getJSONObject(ScenarioConfig.DRONE_SPEC));
         int numVictims = json.getInt(ScenarioConfig.NUM_VICTIMS);
-        double victimMoveSpeedMean = json.getDouble(ScenarioConfig.VICTIM_MOVE_SPEED_MEAN);
-        double victimMoveSpeedStdDev = json.getDouble(ScenarioConfig.VICTIM_MOVE_SPEED_STD_DEV);
+        VictimSpecification victimSpec = VictimSpecification.decode(json.getJSONObject(ScenarioConfig.VICTIM_SPEC));
         double disasterScale = json.getDouble(ScenarioConfig.DISASTER_SCALE);
         Map map = Map.decode(json.getJSONObject(ScenarioConfig.MAP));
         int missionLength = json.getInt(ScenarioConfig.MISSION_LENGTH);
         double stepSize = json.getDouble(ScenarioConfig.STEP_SIZE);
-        return new ScenarioConfig(numVictims, victimMoveSpeedMean, victimMoveSpeedStdDev, disasterScale,
+        return new ScenarioConfig(numDrones, droneSpec, numVictims, victimSpec, disasterScale,
                 map, missionLength, stepSize);
     }
 
@@ -35,10 +39,12 @@ public class ScenarioConfig {
         return ScenarioConfig.decode(new JSONObject(encoding));
     }
 
-    public ScenarioConfig(int numVictims, double victimMoveSpeedMean, double victimMoveSpeedStdDev,
+    public ScenarioConfig(int numDrones, DroneSpecification droneSpec, int numVictims, VictimSpecification victimSpec,
             double disasterScale, Map map, int missionLength, double stepSize) {
+        this.numDrones = numDrones;
+        this.droneSpec = droneSpec;
         this.numVictims = numVictims;
-        this.victimMoveSpeedDistParams = new Tuple<>(victimMoveSpeedMean, victimMoveSpeedStdDev);
+        this.victimSpec = victimSpec;
         this.disasterScaleParam = disasterScale;
         this.map = map;
         this.missionLength = missionLength;
@@ -49,12 +55,20 @@ public class ScenarioConfig {
         return this.disasterScaleParam;
     }
 
+    public DroneSpecification getDroneSpecification() {
+        return this.droneSpec;
+    }
+
     public Map getMap() {
         return this.map;
     }
 
     public int getMissionLength() {
         return this.missionLength;
+    }
+
+    public int getNumberOfDrones() {
+        return this.numDrones;
     }
 
     public int getNumberOfVictims() {
@@ -65,8 +79,8 @@ public class ScenarioConfig {
         return this.stepSize;
     }
 
-    public Tuple<Double, Double> getVictimMoveSpeedDistributionParameters() {
-        return this.victimMoveSpeedDistParams;
+    public VictimSpecification getVictimSpecification() {
+        return this.victimSpec;
     }
 
     public String encode() {
@@ -75,9 +89,10 @@ public class ScenarioConfig {
 
     public JSONObject toJSON() {
         JSONObject json = new JSONObject();
+        json.put(ScenarioConfig.NUM_DRONES, this.numDrones);
+        json.put(ScenarioConfig.DRONE_SPEC, this.droneSpec.toJSON());
         json.put(ScenarioConfig.NUM_VICTIMS, this.numVictims);
-        json.put(ScenarioConfig.VICTIM_MOVE_SPEED_MEAN, this.victimMoveSpeedDistParams.getFirst());
-        json.put(ScenarioConfig.VICTIM_MOVE_SPEED_STD_DEV, this.victimMoveSpeedDistParams.getSecond());
+        json.put(ScenarioConfig.VICTIM_SPEC, this.victimSpec.toJSON());
         json.put(ScenarioConfig.DISASTER_SCALE, this.disasterScaleParam);
         json.put(ScenarioConfig.MAP, this.map.toJSON());
         json.put(ScenarioConfig.MISSION_LENGTH, this.missionLength);
