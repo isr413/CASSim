d5966df9ba4e3d6408586f22c1b18c9a73f6b3b5
isr413 <isr413@utulsa.edu>
2022-02-02 10:34:52 -0600

    Adds mission length and step size to scenario config

--- common/src/main/java/com/seat/rescuesim/common/ScenarioConfig.java
+++ common/src/main/java/com/seat/rescuesim/common/ScenarioConfig.java
@@ -6,14 +6,18 @@ import org.json.JSONObject;
 public class ScenarioConfig {
     private static final String DISASTER_SCALE = "disaster_scale";
     private static final String MAP = "map";
+    private static final String MISSION_LENGTH = "mission_length";
     private static final String NUM_VICTIMS = "num_victims";
+    private static final String STEP_SIZE = "step_size";
     private static final String VICTIM_MOVE_SPEED_MEAN = "victim_move_speed_mean";
     private static final String VICTIM_MOVE_SPEED_STD_DEV = "victim_move_speed_std_dev";
 
-    private int numVictims;
-    private Tuple<Double, Double> victimMoveSpeedDistParams;
     private double disasterScaleParam;
     private Map map;
+    private int missionLength;
+    private int numVictims;
+    private double stepSize;
+    private Tuple<Double, Double> victimMoveSpeedDistParams;
 
     public static ScenarioConfig decode(JSONObject json) throws JSONException {
         int numVictims = json.getInt(ScenarioConfig.NUM_VICTIMS);
@@ -21,7 +25,10 @@ public class ScenarioConfig {
         double victimMoveSpeedStdDev = json.getDouble(ScenarioConfig.VICTIM_MOVE_SPEED_STD_DEV);
         double disasterScale = json.getDouble(ScenarioConfig.DISASTER_SCALE);
         Map map = Map.decode(json.getJSONObject(ScenarioConfig.MAP));
-        return new ScenarioConfig(numVictims, victimMoveSpeedMean, victimMoveSpeedStdDev, disasterScale, map);
+        int missionLength = json.getInt(ScenarioConfig.MISSION_LENGTH);
+        double stepSize = json.getDouble(ScenarioConfig.STEP_SIZE);
+        return new ScenarioConfig(numVictims, victimMoveSpeedMean, victimMoveSpeedStdDev, disasterScale,
+                map, missionLength, stepSize);
     }
 
     public static ScenarioConfig decode(String encoding) {
@@ -29,19 +36,13 @@ public class ScenarioConfig {
     }
 
     public ScenarioConfig(int numVictims, double victimMoveSpeedMean, double victimMoveSpeedStdDev,
-            double disasterScale, Map map) {
+            double disasterScale, Map map, int missionLength, double stepSize) {
         this.numVictims = numVictims;
         this.victimMoveSpeedDistParams = new Tuple<>(victimMoveSpeedMean, victimMoveSpeedStdDev);
         this.disasterScaleParam = disasterScale;
         this.map = map;
-    }
-
-    public int getNumberOfVictims() {
-        return this.numVictims;
-    }
-
-    public Tuple<Double, Double> getVictimMoveSpeedDistributionParameters() {
-        return this.victimMoveSpeedDistParams;
+        this.missionLength = missionLength;
+        this.stepSize = stepSize;
     }
 
     public double getDisasterScaleParameter() {
@@ -50,6 +51,22 @@ public class ScenarioConfig {
 
     public Map getMap() {
         return this.map;
+    }
+
+    public int getMissionLength() {
+        return this.missionLength;
+    }
+
+    public int getNumberOfVictims() {
+        return this.numVictims;
+    }
+
+    public double getStepSize() {
+        return this.stepSize;
+    }
+
+    public Tuple<Double, Double> getVictimMoveSpeedDistributionParameters() {
+        return this.victimMoveSpeedDistParams;
     }
 
     public String encode() {
@@ -63,6 +80,8 @@ public class ScenarioConfig {
         json.put(ScenarioConfig.VICTIM_MOVE_SPEED_STD_DEV, this.victimMoveSpeedDistParams.getSecond());
         json.put(ScenarioConfig.DISASTER_SCALE, this.disasterScaleParam);
         json.put(ScenarioConfig.MAP, this.map.toJSON());
+        json.put(ScenarioConfig.MISSION_LENGTH, this.missionLength);
+        json.put(ScenarioConfig.STEP_SIZE, this.stepSize);
         return json;
     }
 
