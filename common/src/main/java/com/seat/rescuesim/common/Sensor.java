2330192157936c918d4654c772355213fc9cf93e
isr413 <isr413@utulsa.edu>
2022-02-03 14:28:40 -0600

    Fix sensor

--- common/src/main/java/com/seat/rescuesim/common/Sensor.java
+++ common/src/main/java/com/seat/rescuesim/common/Sensor.java
@@ -4,13 +4,13 @@ import org.json.JSONObject;
 
 public class Sensor {
     private static final String SENSOR_ACCURACY = "accuracy";
+    private static final String SENSOR_BATTERY_USAGE = "battery_usage";
     private static final String SENSOR_DELAY = "delay";
-    private static final String SENSOR_FUEL_COST = "fuel_cost";
     private static final String SENSOR_RANGE = "range";
     private static final String SENSOR_TYPE = "type";
 
     private double accuracy;
-    private double cost;
+    private double batteryUsage;
     private double delay;
     private double range;
     private SensorType type;
@@ -18,34 +18,26 @@ public class Sensor {
     public static Sensor decode(JSONObject json) {
         int type = json.getInt(Sensor.SENSOR_TYPE);
         double range = json.getDouble(Sensor.SENSOR_RANGE);
-        double fuelCost = json.getDouble(Sensor.SENSOR_FUEL_COST);
+        double batteryUsage = json.getDouble(Sensor.SENSOR_BATTERY_USAGE);
         double accuracy = json.getDouble(Sensor.SENSOR_ACCURACY);
         double delay = json.getDouble(Sensor.SENSOR_DELAY);
-        return new Sensor(SensorType.values()[type], range, fuelCost, accuracy, delay);
+        return new Sensor(SensorType.values()[type], range, batteryUsage, accuracy, delay);
     }
 
     public static Sensor decode(String encoding) {
         return Sensor.decode(new JSONObject(encoding));
     }
 
-    public Sensor(SensorType type, double range, double fuelCost, double accuracy, double delay) {
+    public Sensor(SensorType type, double range, double batteryUsage, double accuracy, double delay) {
         this.type = type;
         this.range = range;
-        this.cost = fuelCost;
+        this.batteryUsage = batteryUsage;
         this.accuracy = accuracy;
         this.delay = delay;
     }
 
-    public SensorType getType() {
-        return this.type;
-    }
-
-    public double getSensorRange() {
-        return this.range;
-    }
-
-    public double getFuelCost() {
-        return this.cost;
+    public double getBatteryUsage() {
+        return this.batteryUsage;
     }
 
     public double getSensorAccuracy() {
@@ -56,6 +48,14 @@ public class Sensor {
         return this.delay;
     }
 
+    public double getSensorRange() {
+        return this.range;
+    }
+
+    public SensorType getType() {
+        return this.type;
+    }
+
     public String encode() {
         return this.toJSON().toString();
     }
@@ -64,7 +64,7 @@ public class Sensor {
         JSONObject json = new JSONObject();
         json.put(Sensor.SENSOR_TYPE, this.type.getType());
         json.put(Sensor.SENSOR_RANGE, this.range);
-        json.put(Sensor.SENSOR_FUEL_COST, this.cost);
+        json.put(Sensor.SENSOR_BATTERY_USAGE, this.batteryUsage);
         json.put(Sensor.SENSOR_ACCURACY, this.accuracy);
         json.put(Sensor.SENSOR_DELAY, this.delay);
         return json;
@@ -75,8 +75,11 @@ public class Sensor {
     }
 
     public boolean equals(Sensor sensor) {
-        // every sensor should be unique by type in each scenario
         return this.type.equals(sensor.type);
+    }
+
+    public boolean equals(String encoding) {
+        return this.encode().equals(encoding);
     }
 
 }
