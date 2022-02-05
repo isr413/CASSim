aafdfad0f8d36559e83ede98f79bf664b3817c3b
isr413 <isr413@utulsa.edu>
2022-02-05 12:48:21 -0600

    Adds sensors to victim

--- simserver/src/main/java/com/seat/rescuesim/simserver/Victim.java
+++ simserver/src/main/java/com/seat/rescuesim/simserver/Victim.java
@@ -1,12 +1,20 @@
 package com.seat.rescuesim.simserver;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
 import com.seat.rescuesim.common.Debugger;
 import com.seat.rescuesim.common.VictimSpecification;
+import com.seat.rescuesim.common.Sensor;
+import com.seat.rescuesim.common.SensorType;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Victim {
+    private static final String VICTIM_ACTIVE_SENSORS = "active_sensors";
     private static final String VICTIM_BATTERY = "battery_power";
     private static final String VICTIM_ID = "victim_id";
 
@@ -15,7 +23,12 @@ public class Victim {
     public static Victim decode(VictimSpecification victimSpec, JSONObject json) throws JSONException {
         int victimID = json.getInt(Victim.VICTIM_ID);
         double batteryPower = json.getDouble(Victim.VICTIM_BATTERY);
-        return new Victim(victimSpec, victimID, batteryPower);
+        JSONArray jsonSensors = json.getJSONArray(Victim.VICTIM_ACTIVE_SENSORS);
+        ArrayList<SensorType> activeSensors = new ArrayList<>();
+        for (int i = 0; i < jsonSensors.length(); i++) {
+           activeSensors.add(SensorType.values()[jsonSensors.getInt(i)]);
+        }
+        return new Victim(victimSpec, victimID, batteryPower, activeSensors);
     }
 
     public static Victim decode(VictimSpecification victimSpec, String encoding) throws JSONException {
@@ -30,19 +43,68 @@ public class Victim {
         Victim.victimCount++;
     }
 
+    private HashSet<SensorType> activeSensors;
     private double batteryPower;
     private VictimSpecification spec;
     private int victimID;
 
     public Victim(VictimSpecification victimSpec) {
-        this(victimSpec, Victim.getID(), victimSpec.getMaxBatteryPower());
+        this(victimSpec, Victim.getID(), victimSpec.getMaxBatteryPower(), new HashSet<SensorType>());
     }
 
     public Victim(VictimSpecification victimSpec, int victimID, double batteryPower) {
+        this(victimSpec, victimID, batteryPower, new HashSet<SensorType>());
+    }
+
+    public Victim(VictimSpecification victimSpec, int victimID, double batteryPower, SensorType[] activeSensors) {
+        this(victimSpec, victimID, batteryPower, new ArrayList<SensorType>(Arrays.asList(activeSensors)));
+    }
+
+    public Victim(VictimSpecification victimSpec, int victimID, double batteryPower,
+            ArrayList<SensorType> activeSensors) {
+        this(victimSpec, victimID, batteryPower, new HashSet<SensorType>());
+        for (SensorType type : activeSensors) {
+            if (!this.spec.hasSensorWithType(type)) {
+                Debugger.logger.err(String.format("Victim %s should have sensor with type %s",
+                    this.getLabel(), type.getLabel()));
+            } else if (this.hasActiveSensorWithType(type)) {
+                Debugger.logger.err(String.format("Victim has duplicate active sensor of type %s",
+                    this.getLabel(), type.getLabel()));
+            } else {
+                this.activeSensors.add(type);
+            }
+        }
+    }
+
+    public Victim(VictimSpecification victimSpec, int victimID, double batteryPower,
+            HashSet<SensorType> activeSensors) {
         this.spec = victimSpec;
         this.victimID = victimID;
         this.batteryPower = batteryPower;
+        this.activeSensors = activeSensors;
+        for (SensorType type : this.activeSensors) {
+            if (!this.spec.hasSensorWithType(type)) {
+                Debugger.logger.err(String.format("Victim %s should have sensor with type %s",
+                    this.getLabel(), type.getLabel()));
+                this.activeSensors.remove(type);
+            }
+        }
         Victim.updateVictimCount();
+    }
+
+    public boolean activateSensorWithType(SensorType type) {
+        if (!this.hasSensorWithType(type)) {
+            Debugger.logger.err(String.format("Cannot activate sensor %s of victim %s",
+                type.getLabel(), this.getLabel()));
+            return false;
+        }
+        if (this.hasActiveSensorWithType(type)) {
+            Debugger.logger.warn(String.format("Sensor %s of victim %s is already active",
+                type.getLabel(), this.getLabel()));
+            return true;
+        }
+        this.activeSensors.add(type);
+        return true;
     }
 
     public void chargeBattery(double batteryPowerDelta) {
@@ -58,8 +120,49 @@ public class Victim {
         }
     }
 
+    public boolean deactivateSensorWithType(SensorType type) {
+        if (!this.hasSensorWithType(type)) {
+            Debugger.logger.err(String.format("Cannot deactivate sensor %s of victim %s",
+                type.getLabel(), this.getLabel()));
+            return false;
+        }
+        if (!this.hasActiveSensorWithType(type)) {
+            Debugger.logger.warn(String.format("Sensor %s of victim %s is already inactive",
+                type.getLabel(), this.getLabel()));
+            return true;
+        }
+        this.activeSensors.remove(type);
+        return true;
+    }
+
     public void disable() {
         this.batteryPower = 0;
+    }
+
+    public ArrayList<Sensor> getActiveSensors() {
+        ArrayList<Sensor> sensors = new ArrayList<>();
+        for (SensorType type : this.activeSensors) {
+            sensors.add(this.spec.getSensorWithType(type));
+        }
+        return sensors;
+    }
+
+    public Sensor getActiveSensorWithType(SensorType type) {
+        if (!this.spec.hasSensorWithType(type)) {
+            Debugger.logger.err(String.format("No sensor with type %s on victim %s",
+                type.getLabel(), this.getLabel()));
+            return null;
+        }
+        if (!this.hasActiveSensorWithType(type)) {
+            Debugger.logger.err(String.format("Sensor with type %s is inactive on victim %s",
+                type.getLabel(), this.getLabel()));
+            return null;
+        }
+        return this.spec.getSensorWithType(type);
+    }
+
+    public ArrayList<SensorType> getActiveSensorTypes() {
+        return new ArrayList<SensorType>(this.activeSensors);
     }
 
     public double getBatteryPower() {
@@ -70,12 +173,36 @@ public class Victim {
         return String.format("<%d>", this.victimID);
     }
 
+    public ArrayList<Sensor> getSensors() {
+        return this.spec.getSensors();
+    }
+
+    public Sensor getSensorWithType(SensorType type) {
+        return this.spec.getSensorWithType(type);
+    }
+
     public int getVictimID() {
         return this.victimID;
     }
 
     public VictimSpecification getVictimSpecification() {
         return this.spec;
+    }
+
+    public boolean hasActiveSensors() {
+        return !this.activeSensors.isEmpty();
+    }
+
+    public boolean hasActiveSensorWithType(SensorType type) {
+        return this.activeSensors.contains(type);
+    }
+
+    public boolean hasSensors() {
+        return this.spec.hasSensors();
+    }
+
+    public boolean hasSensorWithType(SensorType type) {
+        return this.spec.hasSensorWithType(type);
     }
 
     public boolean isAlive() {
@@ -105,6 +232,11 @@ public class Victim {
         JSONObject json = new JSONObject();
         json.put(Victim.VICTIM_ID, this.victimID);
         json.put(Victim.VICTIM_BATTERY, this.batteryPower);
+        JSONArray jsonSensors = new JSONArray();
+        for (SensorType type : this.activeSensors) {
+            jsonSensors.put(type.getType());
+        }
+        json.put(Victim.VICTIM_ACTIVE_SENSORS, jsonSensors);
         return new JSONObject();
     }
 
