e8536e704bbf4b46fc42350614b3e7dbacd028b0
isr413 <isr413@utulsa.edu>
2022-02-05 12:31:36 -0600

    Adds victim id to victims

--- simserver/src/main/java/com/seat/rescuesim/simserver/Victim.java
+++ simserver/src/main/java/com/seat/rescuesim/simserver/Victim.java
@@ -6,19 +6,46 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Victim {
+    private static final String VICTIM_ID = "victim_id";
 
-    public static Victim decode(JSONObject json, VictimSpecification victimSpec) throws JSONException {
-        return null;
+    private static int victimCount = 0;
+
+    public static Victim decode(VictimSpecification victimSpec, JSONObject json) throws JSONException {
+        int victimID = json.getInt(Victim.VICTIM_ID);
+        return new Victim(victimSpec, victimID);
     }
 
-    public static Victim decode(String encoding, VictimSpecification victimSpec) throws JSONException {
-        return Victim.decode(new JSONObject(encoding), victimSpec);
+    public static Victim decode(VictimSpecification victimSpec, String encoding) throws JSONException {
+        return Victim.decode(victimSpec, new JSONObject(encoding));
+    }
+
+    public static int getID() {
+        return Victim.victimCount;
+    }
+
+    private static void updateVictimCount() {
+        Victim.victimCount++;
     }
 
     private VictimSpecification spec;
+    private int victimID;
 
     public Victim(VictimSpecification victimSpec) {
+        this(victimSpec, Victim.getID());
+    }
+
+    public Victim(VictimSpecification victimSpec, int victimID) {
         this.spec = victimSpec;
+        this.victimID = victimID;
+        Victim.updateVictimCount();
+    }
+
+    public String getLabel() {
+        return String.format("<%d>", this.victimID);
+    }
+
+    public int getVictimID() {
+        return this.victimID;
     }
 
     public VictimSpecification getVictimSpecification() {
@@ -30,6 +57,8 @@ public class Victim {
     }
 
     public JSONObject toJSON() {
+        JSONObject json = new JSONObject();
+        json.put(Victim.VICTIM_ID, this.victimID);
         return new JSONObject();
     }
 
