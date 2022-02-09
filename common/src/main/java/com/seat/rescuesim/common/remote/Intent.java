2ed7f48f02e5853134f1e54ef6fd76e30c2a2afb
Ian Riley <isr413@utulsa.edu>
2022-02-09 14:54:47 -0600

    Adds static class Intent to easily build intentions

---
+++ common/src/main/java/com/seat/rescuesim/common/remote/Intent.java
@@ -0,0 +1,55 @@
+package com.seat.rescuesim.common.remote;
+
+import java.util.HashSet;
+
+import com.seat.rescuesim.common.SensorType;
+import com.seat.rescuesim.common.math.Vector;
+import com.seat.rescuesim.common.remote.intent.*;
+
+public class Intent {
+
+    public static Intention Activate(HashSet<SensorType> sensors) {
+        return new ActivateIntention(sensors);
+    }
+
+    public static Intention Deactivate(HashSet<SensorType> sensors) {
+        return new DeactivateIntention(sensors);
+    }
+
+    public static Intention Done() {
+        return new DoneIntention();
+    }
+
+    public static Intention Goto(Vector location) {
+        return new GotoIntention(location);
+    }
+
+    public static Intention Goto(Vector location, double maxAcceleration) {
+        return new GotoIntention(location, maxAcceleration);
+    }
+
+    public static Intention Goto(Vector location, double maxAcceleration, double maxVelocity) {
+        return new GotoIntention(location, maxAcceleration, maxVelocity);
+    }
+
+    public static Intention Move(Vector jerk) {
+        return new MoveIntention(jerk);
+    }
+
+    public static Intention None() {
+        return new DoneIntention();
+    }
+
+    public static Intention Shutdown() {
+        return new DoneIntention();
+    }
+
+    public static Intention Startup() {
+        return new DoneIntention();
+    }
+
+    public static Intention Stop() {
+        return new DoneIntention();
+    }
+
+}
