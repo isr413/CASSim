ae785e9f504602a8ea757e21922c85cddc978e05
Ian Riley <isr413@utulsa.edu>
2022-02-08 23:02:37 -0600

    Adds SerializableEnum interface

---
+++ common/src/main/java/com/seat/rescuesim/common/util/SerializableEnum.java
@@ -0,0 +1,24 @@
+package com.seat.rescuesim.common.util;
+
+/** An interface to support serializable enums. */
+public interface SerializableEnum {
+
+    /** Must be implemented to return the enum's type (enum constant). */
+    int getType();
+
+    /** Returns the String encoding of the enum. */
+    default String encode() {
+        return String.valueOf(this.getType());
+    }
+
+    /** Returns true if the encoded SerializableEnum has the same enum constant. */
+    default boolean equals(String encoding) {
+        return this.encode().equals(encoding);
+    }
+
+    /** Returns the String representation of the enum. */
+    default String getLabel() {
+        return String.format("<%d>", this.getType());
+    }
+
+}
