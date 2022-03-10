87803c0b744952f949eb97ef560ee9bb1d054b53
isr413 <isr413@utulsa.edu>
2022-03-09 21:38:43 -0600

    Adds socket support

--- client/src/main/java/com/seat/rescuesim/client/App.java
+++ client/src/main/java/com/seat/rescuesim/client/App.java
@@ -1,9 +1,34 @@
 package com.seat.rescuesim.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+import com.seat.rescuesim.client.sandbox.ACSOS;
 
 public class App {
-    public static void main(String[] args) {
 
+    private static final InetAddress ADDRESS = InetAddress.getLoopbackAddress();
+    private static final int PORT = 8080;
+
+    private static Application GetApplication(String[] args) {
+        return new ACSOS(args);
+    }
+
+    public static void main(String[] args) {
+        Application app = GetApplication(args);
+        System.out.println(app.getScenario().encode());
+        try {
+            Socket socket = new Socket(ADDRESS, PORT);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(app.getScenario().encode());
+            socket.close();
+        } catch(IOException e) {
+            System.err.println(e);
+        }
     }
 
 }
