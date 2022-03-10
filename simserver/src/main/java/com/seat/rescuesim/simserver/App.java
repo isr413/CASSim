df9ce4940bf9d4b022f312b8591d8be3c712d361
isr413 <isr413@utulsa.edu>
2022-03-09 22:17:08 -0600

    Adds server socket

--- simserver/src/main/java/com/seat/rescuesim/simserver/App.java
+++ simserver/src/main/java/com/seat/rescuesim/simserver/App.java
@@ -1,13 +1,46 @@
 package com.seat.rescuesim.simserver;
 
-/**
- * Hello world!
- *
- */
-public class App
-{
-    public static void main( String[] args )
-    {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+import com.seat.rescuesim.common.util.Debugger;
+
+public class App {
+
+    private static final int PORT = 8080;
+
+    private static void waitForScenario(BufferedReader in) {
+        Debugger.logger.info("Waiting for scenario config...");
+        boolean waiting = true;
+        String message = null;
+        try {
+            while (waiting) {
+                if (in.ready()) {
+                    message = in.readLine();
+                    System.out.println(message);
+                    waiting = false;
+                }
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
     }
+
+    public static void main(String[] args) {
+        try {
+            Debugger.logger.info("Sim server starting...");
+            ServerSocket server = new ServerSocket(PORT);
+            Debugger.logger.info(String.format("Listening on port=%d", PORT));
+            Socket client = server.accept();
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            waitForScenario(in);
+            server.close();
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+
 }
