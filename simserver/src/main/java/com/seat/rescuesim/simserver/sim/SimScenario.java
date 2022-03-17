7c3780e55bcd239e4fe4ca36587f7b994dd9d0c6
isr413 <isr413@utulsa.edu>
2022-03-17 12:27:13 -0500

    Bounces passive victims within bounds

--- simserver/src/main/java/com/seat/rescuesim/simserver/sim/SimScenario.java
+++ simserver/src/main/java/com/seat/rescuesim/simserver/sim/SimScenario.java
@@ -10,7 +10,9 @@ import com.seat.rescuesim.common.ScenarioConfig;
 import com.seat.rescuesim.common.SnapStatus;
 import com.seat.rescuesim.common.Snapshot;
 import com.seat.rescuesim.common.map.Map;
+import com.seat.rescuesim.common.math.Vector;
 import com.seat.rescuesim.common.remote.RemoteState;
+import com.seat.rescuesim.common.remote.intent.Intent;
 import com.seat.rescuesim.common.remote.intent.Intention;
 import com.seat.rescuesim.common.util.Debugger;
 import com.seat.rescuesim.common.victim.VictimConfig;
@@ -40,6 +42,105 @@ public class SimScenario {
         this.dynamicVictims = new HashMap<>();
         this.passiveVictims = new HashMap<>();
         this.initVictims();
+    }
+
+    private boolean boundsCheck(Vector nextLocation) {
+        return 0 <= nextLocation.getX() && nextLocation.getX() <= this.getMapWidth() && 0 <= nextLocation.getY() &&
+            nextLocation.getY() <= this.getMapHeight();
+    }
+
+    public void enforceBounds(SimVictim victim, Vector prevLocation) {
+        int impactType = this.getImpactType(prevLocation, victim.getLocation());
+        Vector impactPoint = this.getImpactPoint(prevLocation, victim.getLocation(), impactType);
+        if (impactPoint == null) {
+            Debugger.logger.err(String.format("Could not compute bounce location for %s on victim %s",
+                victim.getLocation().toString(), victim.getLabel()));
+            return;
+        }
+        Vector bounce = this.getBounceLocation(impactPoint, victim.getLocation(), impactType);
+        if (bounce == null) {
+            Debugger.logger.err(String.format("Could not compute bounce location for %s on victim %s",
+                victim.getLocation().toString(), victim.getLabel()));
+            return;
+        }
+        System.out.println(String.format("%f %f %f", prevLocation.getX(), prevLocation.getY(), prevLocation.getZ()));
+        System.out.println(String.format("%f %f %f", victim.getLocation().getX(), victim.getLocation().getY(), victim.getLocation().getZ()));
+        System.out.println(String.format("%f %f %f", bounce.getX(), bounce.getY(), bounce.getZ()));
+        victim.setLocation(bounce);
+        Vector newVelocity = Vector.scale(Vector.subtract(bounce, impactPoint).getUnitVector(),
+            victim.getVelocity().getMagnitude());
+        victim.setVelocity(newVelocity);
+        Vector newAcceleration = Vector.scale(Vector.subtract(bounce, impactPoint).getUnitVector(),
+            victim.getAcceleration().getMagnitude());
+        victim.setAcceleration(newAcceleration);
+    }
+
+    private Vector getBounceLocation(Vector impactPoint, Vector nextLocation, int impactType) {
+        double deltaX = nextLocation.getX() - impactPoint.getX();
+        double deltaY = nextLocation.getY() - impactPoint.getY();
+        switch (impactType) {
+            case 0:
+            case 2:
+            case 4:
+            case 6:
+                return new Vector(impactPoint.getX() - deltaX, impactPoint.getY() - deltaY);
+            case 1:
+            case 5:
+                return new Vector(impactPoint.getX() + deltaX, impactPoint.getY() - deltaY);
+            case 3:
+            case 7:
+                return new Vector(impactPoint.getX() - deltaX, impactPoint.getY() + deltaY);
+            default:
+                return null;
+        }
+    }
+
+    private Vector getImpactPoint(Vector location, Vector nextLocation, int impactType) {
+        double slope = Vector.slope(location, nextLocation);
+        switch (impactType) {
+            case 0:
+                return new Vector(0, 0);
+            case 2:
+                return new Vector(this.getMapWidth(), 0);
+            case 1:
+                double deltaX = (Double.isFinite(slope) && slope != 0) ? (0 - location.getY()) / slope : 0;
+                return new Vector(location.getX() + deltaX, 0);
+            case 4:
+                return new Vector(this.getMapWidth(), this.getMapHeight());
+            case 3:
+                double deltaY = (Double.isFinite(slope)) ? (this.getMapWidth() - location.getX()) * slope : 0;
+                return new Vector(this.getMapWidth(), location.getY() + deltaY);
+            case 6:
+                return new Vector(0, this.getMapHeight());
+            case 5:
+                deltaX = (Double.isFinite(slope) && slope != 0) ? (this.getMapHeight()-location.getY()) / slope : 0;
+                return new Vector(location.getX() + deltaX, this.getMapHeight());
+            case 7:
+                deltaY = (Double.isFinite(slope)) ? (0 - location.getX()) * slope : 0;
+                return new Vector(0, location.getY() + deltaY);
+            default:
+                return null;
+        }
+    }
+
+    public int getImpactType(Vector location, Vector nextLocation) {
+        if (nextLocation.getX() < 0 && nextLocation.getY() < 0) {
+            return 0;
+        } else if (this.getMapWidth() < nextLocation.getX() && nextLocation.getY() < 0) {
+            return 2;
+        } else if (nextLocation.getY() < 0) {
+            return 1;
+        } else if (this.getMapWidth() < nextLocation.getX() && this.getMapHeight() < nextLocation.getY()) {
+            return 4;
+        } else if (this.getMapWidth() < nextLocation.getX()) {
+            return 3;
+        } else if (nextLocation.getX() < 0 && this.getMapHeight() < nextLocation.getY()) {
+            return 6;
+        } else if (this.getMapHeight() < nextLocation.getY()) {
+            return 5;
+        } else {
+            return 7;
+        }
     }
 
     private void initVictims() {
@@ -166,7 +267,18 @@ public class SimScenario {
         }
         Debugger.logger.info(String.format("Updating passive remotes %s", this.passiveVictims.keySet().toString()));
         for (SimVictim victim : this.passiveVictims.values()) {
-            victim.update(stepSize);
+            Vector location = victim.getLocation();
+            if (!this.boundsCheck(location)) {
+                Debugger.logger.err(String.format("Victim %s out of bounds at %s",
+                    victim.getLabel(), location.toString()));
+                continue;
+            }
+            ArrayList<Intention> victimIntentions = new ArrayList<>();
+            victimIntentions.add(Intent.None());
+            victim.update(victimIntentions, stepSize);
+            if (!this.boundsCheck(victim.getLocation())) {
+                this.enforceBounds(victim, location);
+            }
         }
         return this.getSnapshot();
     }
--- simserver/src/main/java/com/seat/rescuesim/simserver/sim/SimVictim.java
+++ simserver/src/main/java/com/seat/rescuesim/simserver/sim/SimVictim.java
@@ -3,6 +3,7 @@ package com.seat.rescuesim.simserver.sim;
 import java.util.ArrayList;
 
 import com.seat.rescuesim.common.math.Vector;
+import com.seat.rescuesim.common.remote.intent.Intent;
 import com.seat.rescuesim.common.remote.intent.Intention;
 import com.seat.rescuesim.common.sensor.SensorState;
 import com.seat.rescuesim.common.victim.VictimSpec;
@@ -46,6 +47,25 @@ public class SimVictim {
         return this.location;
     }
 
+    public Vector getNextLocation() {
+        return this.getNextLocation(new Vector());
+    }
+
+    public Vector getNextLocation(Vector jerk) {
+        if (this.spec.getMaxJerk() < jerk.getMagnitude()) {
+            jerk = Vector.scale(jerk.getUnitVector(), this.spec.getMaxJerk());
+        }
+        Vector nextAcceleration = Vector.add(this.acceleration, jerk);
+        if (this.spec.getMaxAcceleration() < nextAcceleration.getMagnitude()) {
+            nextAcceleration = Vector.scale(nextAcceleration.getUnitVector(), this.spec.getMaxAcceleration());
+        }
+        Vector nextVelocity = Vector.add(this.velocity, nextAcceleration);
+        if (this.spec.getMaxVelocity() < nextVelocity.getMagnitude()) {
+            nextVelocity = Vector.scale(nextVelocity.getUnitVector(), this.spec.getMaxVelocity());
+        }
+        return Vector.add(this.location, nextVelocity);
+    }
+
     public VictimSpec getSpec() {
         return this.spec;
     }
@@ -70,12 +90,29 @@ public class SimVictim {
         return this.getLabel();
     }
 
+    public void setAcceleration(Vector acceleration) {
+        this.acceleration = acceleration;
+    }
+
+    public void setBattery(double battery) {
+        this.battery = battery;
+    }
+
+    public void setLocation(Vector location) {
+        this.location = location;
+    }
+
+    public void setVelocity(Vector velocity) {
+        this.velocity = velocity;
+    }
+
     public VictimState update(double stepSize) {
         return this.update(null, stepSize);
     }
 
     public VictimState update(ArrayList<Intention> intentions, double stepSize) {
-        if (intentions == null || intentions.isEmpty()) {
+        if (intentions == null || intentions.isEmpty() ||
+                (intentions.size() == 1 && intentions.get(0).equals(Intent.None()))) {
             this.velocity = Vector.add(this.velocity, Vector.scale(this.acceleration, stepSize));
             this.location = Vector.add(this.location, this.velocity);
         }
