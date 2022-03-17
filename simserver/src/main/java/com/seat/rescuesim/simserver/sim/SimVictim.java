80be065260c7e69c9926487d3a54889d5526c32c
isr413 <isr413@utulsa.edu>
2022-03-17 14:51:00 -0500

    Handles stop, shutdown, move, and goto intentions

--- simserver/src/main/java/com/seat/rescuesim/simserver/sim/SimVictim.java
+++ simserver/src/main/java/com/seat/rescuesim/simserver/sim/SimVictim.java
@@ -3,8 +3,10 @@ package com.seat.rescuesim.simserver.sim;
 import java.util.ArrayList;
 
 import com.seat.rescuesim.common.math.Vector;
+import com.seat.rescuesim.common.remote.intent.GotoIntention;
 import com.seat.rescuesim.common.remote.intent.Intent;
 import com.seat.rescuesim.common.remote.intent.Intention;
+import com.seat.rescuesim.common.remote.intent.MoveIntention;
 import com.seat.rescuesim.common.sensor.SensorState;
 import com.seat.rescuesim.common.victim.VictimSpec;
 import com.seat.rescuesim.common.victim.VictimState;
@@ -45,11 +47,83 @@ public class SimVictim extends KineticSimAsset {
         );
     }
 
+    private void updateAcceleration(Vector jerk) {
+        this.updateAcceleration(jerk, this.getSpec().getMaxAcceleration());
+    }
+
+    private void updateAcceleration(Vector jerk, double maxAcceleration) {
+        maxAcceleration = Math.min(maxAcceleration, this.getSpec().getMaxAcceleration());
+        if (this.getSpec().getMaxJerk() < jerk.getMagnitude()) {
+            jerk = Vector.scale(jerk.getUnitVector(), this.getSpec().getMaxJerk());
+        }
+        this.acceleration = Vector.add(this.acceleration, jerk);
+        if (maxAcceleration < this.acceleration.getMagnitude()) {
+            this.acceleration = Vector.scale(this.acceleration.getUnitVector(), maxAcceleration);
+        }
+    }
+
+    private void updateLocation() {
+        this.location = Vector.add(this.location, this.velocity);
+    }
+
+    private void updateLocationTo(Vector location, double maxVelocity, double maxAcceleration) {
+        Vector jerk = Vector.subtract(location, this.getNextLocation());
+        this.updateAcceleration(jerk, maxAcceleration);
+        this.updateVelocity(maxVelocity);
+        this.updateLocation();
+    }
+
+    private void updateVelocity() {
+        this.updateVelocity(this.getSpec().getMaxVelocity());
+    }
+
+    private void updateVelocity(double maxVelocity) {
+        maxVelocity = Math.min(maxVelocity, this.getSpec().getMaxVelocity());
+        this.velocity = Vector.add(this.velocity, this.acceleration);
+        if (maxVelocity < this.velocity.getMagnitude()) {
+            this.velocity = Vector.scale(this.velocity.getUnitVector(), maxVelocity);
+        }
+    }
+
+    private void updateVelocityTo(Vector nextVelocity) {
+        Vector jerk = Vector.subtract(nextVelocity, Vector.add(this.velocity, this.acceleration));
+        this.updateAcceleration(jerk);
+        this.updateVelocity();
+    }
+
     public VictimState update(ArrayList<Intention> intentions, double stepSize) throws SimException {
         if (intentions == null || intentions.isEmpty() ||
                 (intentions.size() == 1 && intentions.get(0).equals(Intent.None()))) {
-            this.velocity = Vector.add(this.velocity, Vector.scale(this.acceleration, stepSize));
-            this.location = Vector.add(this.location, this.velocity);
+            this.updateVelocity();
+            this.updateLocation();
+            return this.getState();
+        }
+        for (Intention intent : intentions) {
+            switch (intent.getIntentionType()) {
+                case DONE:
+                case STOP:
+                    this.updateVelocityTo(new Vector());
+                    this.updateLocation();
+                    continue;
+                case SHUTDOWN:
+                    this.updateVelocityTo(new Vector());
+                    this.updateLocation();
+                    continue;
+                case MOVE:
+                    this.updateAcceleration(((MoveIntention) intent).getJerk());
+                    this.updateVelocity();
+                    this.updateLocation();
+                    continue;
+                case GOTO:
+                    this.updateLocationTo(
+                        ((GotoIntention) intent).getLocation(),
+                        ((GotoIntention) intent).getMaxVelocity(),
+                        ((GotoIntention) intent).getMaxAcceleration()
+                    );
+                    continue;
+                default:
+                    continue;
+            }
         }
         return this.getState();
     }
