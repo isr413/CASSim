666572ac9ebc974c4c5662c8c3c2fe73fda69ea5
Ian Riley <isr413@utulsa.edu>
2022-03-21 14:08:35 -0500

    Adds remaining remote factory methods

--- common/src/main/java/com/seat/rescuesim/common/util/RemoteFactory.java
+++ common/src/main/java/com/seat/rescuesim/common/util/RemoteFactory.java
@@ -1,28 +1,67 @@
 package com.seat.rescuesim.common.util;
 
+import com.seat.rescuesim.common.base.BaseConfig;
+import com.seat.rescuesim.common.base.BaseSpec;
 import com.seat.rescuesim.common.base.BaseState;
+import com.seat.rescuesim.common.drone.DroneConfig;
+import com.seat.rescuesim.common.drone.DroneSpec;
 import com.seat.rescuesim.common.drone.DroneState;
 import com.seat.rescuesim.common.json.*;
+import com.seat.rescuesim.common.remote.RemoteConfig;
 import com.seat.rescuesim.common.remote.RemoteConst;
+import com.seat.rescuesim.common.remote.RemoteSpec;
 import com.seat.rescuesim.common.remote.RemoteState;
 import com.seat.rescuesim.common.remote.RemoteType;
+import com.seat.rescuesim.common.victim.VictimConfig;
+import com.seat.rescuesim.common.victim.VictimSpec;
 import com.seat.rescuesim.common.victim.VictimState;
 
 public class RemoteFactory {
 
-    public static RemoteType decodeRemoteType(JSONObject json) throws JSONException {
-        return RemoteType.values()[json.getInt(RemoteConst.REMOTE_TYPE)];
-    }
-
-    public static RemoteType decodeRemoteType(JSONOption option) throws JSONException {
-        if (option.isSomeObject()) {
-            return RemoteFactory.decodeRemoteType(option.someObject());
+    public static RemoteConfig decodeRemoteConfig(JSONObject json) throws JSONException {
+        RemoteType remoteType = RemoteFactory.decodeRemoteType(json);
+        if (remoteType.equals(RemoteType.BASE)) {
+            return new BaseConfig(json);
+        } else if (remoteType.equals(RemoteType.DRONE)) {
+            return new DroneConfig(json);
+        } else if (remoteType.equals(RemoteType.VICTIM)) {
+            return new VictimConfig(json);
         }
-        throw new JSONException(String.format("Cannot decode remote type of %s", option.toString()));
+        throw new JSONException(String.format("Cannot decode remote config of %s", json.toString()));
     }
 
-    public static RemoteType decodeRemoteType(String encoding) throws JSONException {
-        return RemoteFactory.decodeRemoteType(JSONOption.String(encoding));
+    public static RemoteConfig decodeRemoteConfig(JSONOption option) throws JSONException {
+        if (option.isSomeObject()) {
+            return RemoteFactory.decodeRemoteConfig(option.someObject());
+        }
+        throw new JSONException(String.format("Cannot decode remote config of %s", option.toString()));
+    }
+
+    public static RemoteConfig decodeRemoteConfig(String encoding) throws JSONException {
+        return RemoteFactory.decodeRemoteConfig(JSONOption.String(encoding));
+    }
+
+    public static RemoteSpec decodeRemoteSpec(JSONObject json) throws JSONException {
+        RemoteType remoteType = RemoteFactory.decodeRemoteType(json);
+        if (remoteType.equals(RemoteType.BASE)) {
+            return new BaseSpec(json);
+        } else if (remoteType.equals(RemoteType.DRONE)) {
+            return new DroneSpec(json);
+        } else if (remoteType.equals(RemoteType.VICTIM)) {
+            return new VictimSpec(json);
+        }
+        throw new JSONException(String.format("Cannot decode remote spec of %s", json.toString()));
+    }
+
+    public static RemoteSpec decodeRemoteSpec(JSONOption option) throws JSONException {
+        if (option.isSomeObject()) {
+            return RemoteFactory.decodeRemoteSpec(option.someObject());
+        }
+        throw new JSONException(String.format("Cannot decode remote spec of %s", option.toString()));
+    }
+
+    public static RemoteSpec decodeRemoteSpec(String encoding) throws JSONException {
+        return RemoteFactory.decodeRemoteSpec(JSONOption.String(encoding));
     }
 
     public static RemoteState decodeRemoteState(JSONObject json) throws JSONException {
@@ -46,6 +85,21 @@ public class RemoteFactory {
 
     public static RemoteState decodeRemoteState(String encoding) throws JSONException {
         return RemoteFactory.decodeRemoteState(JSONOption.String(encoding));
+    }
+
+    public static RemoteType decodeRemoteType(JSONObject json) throws JSONException {
+        return RemoteType.values()[json.getInt(RemoteConst.REMOTE_TYPE)];
+    }
+
+    public static RemoteType decodeRemoteType(JSONOption option) throws JSONException {
+        if (option.isSomeObject()) {
+            return RemoteFactory.decodeRemoteType(option.someObject());
+        }
+        throw new JSONException(String.format("Cannot decode remote type of %s", option.toString()));
+    }
+
+    public static RemoteType decodeRemoteType(String encoding) throws JSONException {
+        return RemoteFactory.decodeRemoteType(JSONOption.String(encoding));
     }
 
 }
--- common/src/main/java/com/seat/rescuesim/common/util/SensorFactory.java
+++ common/src/main/java/com/seat/rescuesim/common/util/SensorFactory.java
@@ -11,21 +11,6 @@ import org.json.JSONException;
 
 public class SensorFactory {
 
-    public static SensorType decodeSensorType(JSONObject json) throws JSONException {
-        return SensorType.values()[json.getInt(SensorConst.SENSOR_TYPE)];
-    }
-
-    public static SensorType decodeSensorType(JSONOption option) throws JSONException {
-        if (option.isSomeObject()) {
-            return SensorFactory.decodeSensorType(option.someObject());
-        }
-        throw new JSONException(String.format("Cannot decode sensor type of %s", option.toString()));
-    }
-
-    public static SensorType decodeSensorType(String encoding) throws JSONException {
-        return SensorFactory.decodeSensorType(JSONOption.String(encoding));
-    }
-
     public static SensorState decodeSensorState(JSONObject json) throws JSONException {
         SensorType sensorType = SensorFactory.decodeSensorType(json);
         if (sensorType.equals(SensorType.Comms)) {
@@ -48,6 +33,21 @@ public class SensorFactory {
 
     public static SensorState decodeSensorState(String encoding) throws JSONException {
         return SensorFactory.decodeSensorState(JSONOption.String(encoding));
+    }
+
+    public static SensorType decodeSensorType(JSONObject json) throws JSONException {
+        return SensorType.values()[json.getInt(SensorConst.SENSOR_TYPE)];
+    }
+
+    public static SensorType decodeSensorType(JSONOption option) throws JSONException {
+        if (option.isSomeObject()) {
+            return SensorFactory.decodeSensorType(option.someObject());
+        }
+        throw new JSONException(String.format("Cannot decode sensor type of %s", option.toString()));
+    }
+
+    public static SensorType decodeSensorType(String encoding) throws JSONException {
+        return SensorFactory.decodeSensorType(JSONOption.String(encoding));
     }
 
 }
