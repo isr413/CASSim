a35f4d0eb6e4a650d13f3d19be0ea30b8d7c73b2
Ian Riley <isr413@utulsa.edu>
2022-03-23 14:58:57 -0500

    Adds Application interface to common

---
+++ common/src/main/java/com/seat/rescuesim/common/app/Application.java
@@ -0,0 +1,44 @@
+package com.seat.rescuesim.common.app;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import com.seat.rescuesim.common.ScenarioConfig;
+import com.seat.rescuesim.common.Snapshot;
+import com.seat.rescuesim.common.map.Map;
+import com.seat.rescuesim.common.remote.RemoteConfig;
+import com.seat.rescuesim.common.remote.RemoteController;
+
+public interface Application {
+
+    double getDisasterScale();
+
+    Map getMap();
+
+    int getMissionLength();
+
+    String getScenarioID();
+
+    default long getSeed() {
+        return new Random().nextLong();
+    }
+
+    default ScenarioConfig getScenarioConfig() {
+        return new ScenarioConfig(
+            getScenarioID(),
+            getSeed(),
+            getMap(),
+            getDisasterScale(),
+            getMissionLength(),
+            getStepSize(),
+            getRemoteConfigs()
+        );
+    }
+
+    ArrayList<RemoteConfig> getRemoteConfigs();
+
+    double getStepSize();
+
+    ArrayList<RemoteController> update(Snapshot snap);
+
+}
