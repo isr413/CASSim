8da12d427971700c1e76b7b7f6c0c3c7535a829a
Ian Riley <isr413@utulsa.edu>
2022-03-31 11:03:39 -0500

    Adds category flags to RemoteType and SensorType

--- common/src/main/java/com/seat/rescuesim/common/remote/RemoteType.java
+++ common/src/main/java/com/seat/rescuesim/common/remote/RemoteType.java
@@ -4,23 +4,37 @@ import com.seat.rescuesim.common.util.SerializableEnum;
 
 /** A serializable enumeration to denote types of Remotes. */
 public enum RemoteType implements SerializableEnum {
-    NONE(0),
-    BASE(1),
-    DRONE(2),
-    VICTIM(3);
+    NONE(0, RemoteType.NONE_REMOTE),
+    BASE(1, RemoteType.STATIC_REMOTE),
+    DRONE(2, RemoteType.KINETIC_REMOTE),
+    VICTIM(3, RemoteType.KINETIC_REMOTE);
 
+    public static final int NONE_REMOTE = 0;
+    public static final int STATIC_REMOTE = 1;
+    public static final int KINETIC_REMOTE = 1 << 1;
+
+    private int categories;
     private int type;
 
-    private RemoteType(int type) {
+    private RemoteType(int type, int categories) {
         this.type = type;
+        this.categories = categories;
     }
 
     public boolean equals(RemoteType type) {
         return this.type == type.type;
     }
 
+    public int getCategories() {
+        return this.categories;
+    }
+
     public int getType() {
         return this.type;
+    }
+
+    public boolean hasCategory(int categoryFlags) {
+        return (this.categories & categoryFlags) > 0;
     }
 
     public String toString() {
--- common/src/main/java/com/seat/rescuesim/common/sensor/SensorType.java
+++ common/src/main/java/com/seat/rescuesim/common/sensor/SensorType.java
@@ -4,23 +4,42 @@ import com.seat.rescuesim.common.util.SerializableEnum;
 
 /** A serializable enum to denote types of sensors. */
 public enum SensorType implements SerializableEnum {
-    None(0),
-    Comms(1),
-    Monitor(2),
-    Vision(3);
+    None(0, SensorType.NONE_SENSOR),
+    Comms(1, SensorType.COMMS_SENSOR),
+    Monitor(2, SensorType.MONITOR_SENSOR),
+    Vision(3, SensorType.VISION_SENSOR),
+    Bluetooth(4, SensorType.COMMS_SENSOR),
+    LTERadio(5, SensorType.COMMS_SENSOR),
+    HRVM(6, SensorType.MONITOR_SENSOR),
+    CMOSCamera(7, SensorType.VISION_SENSOR);
 
+    public static final int NONE_SENSOR = 0;
+    public static final int COMMS_SENSOR = 1;
+    public static final int MONITOR_SENSOR = 1 << 1;
+    public static final int VISION_SENSOR = 1 << 2;
+
+    private int categories;
     private int type;
 
-    private SensorType(int type) {
+    private SensorType(int type, int categories) {
         this.type = type;
+        this.categories = categories;
     }
 
     public boolean equals(SensorType type) {
         return this.type == type.type;
     }
 
+    public int getCategories() {
+        return this.categories;
+    }
+
     public int getType() {
         return this.type;
+    }
+
+    public boolean hasCategory(int categoryFlags) {
+        return (this.categories & categoryFlags) > 0;
     }
 
     public String toString() {
