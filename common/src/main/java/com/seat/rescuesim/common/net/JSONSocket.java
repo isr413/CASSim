d38fe884a541c877cbac4449117791a54d9bbfb0
Ian Riley <isr413@utulsa.edu>
2022-04-07 15:45:10 -0500

    Standardizes JSONSocket on CoreException

--- common/src/main/java/com/seat/rescuesim/common/net/JSONSocket.java
+++ common/src/main/java/com/seat/rescuesim/common/net/JSONSocket.java
@@ -25,28 +25,40 @@ public class JSONSocket {
     protected static final InetAddress DEFAULT_ADDRESS = InetAddress.getLoopbackAddress();
     protected static final int DEFAULT_PORT = 8080;
 
-    public static JSONSocket Client() throws IOException {
+    public static JSONSocket Client() throws CoreException {
         return JSONSocket.Client(JSONSocket.DEFAULT_ADDRESS, JSONSocket.DEFAULT_PORT);
     }
 
-    public static JSONSocket Client(int port) throws IOException {
+    public static JSONSocket Client(int port) throws CoreException {
         return JSONSocket.Client(JSONSocket.DEFAULT_ADDRESS, port);
     }
 
-    public static JSONSocket Client(InetAddress address, int port) throws IOException {
-        return new JSONSocket(new Socket(address, port));
+    public static JSONSocket Client(InetAddress address, int port) throws CoreException {
+        try {
+            return new JSONSocket(new Socket(address, port));
+        } catch(IOException e) {
+            throw new CoreException(e.toString());
+        }
     }
 
-    public static JSONSocket Client(String address, int port) throws IOException {
-        return new JSONSocket(new Socket(address, port));
+    public static JSONSocket Client(String address, int port) throws CoreException {
+        try {
+            return new JSONSocket(new Socket(address, port));
+        } catch(IOException e) {
+            throw new CoreException(e.toString());
+        }
     }
 
-    public static JSONSocket Server() throws IOException {
+    public static JSONSocket Server() throws CoreException {
         return JSONSocket.Server(JSONSocket.DEFAULT_PORT);
     }
 
-    public static JSONSocket Server(int port) throws IOException {
-        return new JSONSocket(new ServerSocket(port));
+    public static JSONSocket Server(int port) throws CoreException {
+        try {
+            return new JSONSocket(new ServerSocket(port));
+        } catch(IOException e) {
+            throw new CoreException(e.toString());
+        }
     }
 
     private ServerSocket serverSocket;
@@ -54,26 +66,38 @@ public class JSONSocket {
     private BufferedReader in;
     private PrintWriter out;
 
-    private JSONSocket(ServerSocket socket) throws IOException {
-        this.serverSocket = socket;
-        this.clientSocket = this.serverSocket.accept();
-        this.in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
-        this.out = new PrintWriter(this.clientSocket.getOutputStream(), true);
+    private JSONSocket(ServerSocket socket) throws CoreException {
+        try {
+            this.serverSocket = socket;
+            this.clientSocket = this.serverSocket.accept();
+            this.in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+            this.out = new PrintWriter(this.clientSocket.getOutputStream(), true);
+        } catch (IOException e) {
+            throw new CoreException(e.toString());
+        }
     }
 
-    private JSONSocket(Socket socket) throws IOException {
-        this.serverSocket = null;
-        this.clientSocket = socket;
-        this.in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
-        this.out = new PrintWriter(this.clientSocket.getOutputStream(), true);
+    private JSONSocket(Socket socket) throws CoreException {
+        try {
+            this.serverSocket = null;
+            this.clientSocket = socket;
+            this.in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+            this.out = new PrintWriter(this.clientSocket.getOutputStream(), true);
+        } catch (IOException e) {
+            throw new CoreException(e.toString());
+        }
     }
 
-    public void close() throws IOException {
-        this.out.close();
-        this.in.close();
-        this.clientSocket.close();
-        if (this.serverSocket != null) {
-            this.serverSocket.close();
+    public void close() throws CoreException {
+        try {
+            this.out.close();
+            this.in.close();
+            this.clientSocket.close();
+            if (this.serverSocket != null) {
+                this.serverSocket.close();
+            }
+        } catch (IOException e) {
+            throw new CoreException(e.toString());
         }
     }
 
@@ -83,45 +107,48 @@ public class JSONSocket {
             this.clientSocket.getInetAddress().toString();
     }
 
-    public JSONOption getInputBlocking() throws CoreException, IOException {
-        while (true) {
-            if (this.in.ready()) {
-                String encoding = in.readLine();
-                JSONOption option = JSONOption.String(encoding);
-                if (option.isNone()) {
-                    throw new CoreException(encoding);
+    public JSONOption getInputBlocking() throws CoreException {
+        try {
+            while (true) {
+                if (this.in.ready()) {
+                    String encoding = in.readLine();
+                    JSONOption option = JSONOption.String(encoding);
+                    if (option.isNone()) {
+                        throw new CoreException(encoding);
+                    }
+                    return option;
                 }
-                return option;
             }
+        } catch(IOException e) {
+            throw new CoreException(e.toString());
         }
     }
 
-    public HashMap<String, IntentionSet> getIntentionsBlocking() throws CoreException, IOException {
-        JSONArray json;
+    public HashMap<String, IntentionSet> getIntentionsBlocking() throws CoreException {
         try {
-            json = this.getInputBlocking().someArray();
+            JSONArray json = this.getInputBlocking().someArray();
+            HashMap<String, IntentionSet> controllers = new HashMap<>();
+            for (int i = 0; i < json.length(); i++) {
+                IntentionSet controller = new IntentionSet(json.getJSONOption(i));
+                if (controller.hasIntentions()) {
+                    controllers.put(controller.getRemoteID(), controller);
+                }
+            }
+            return controllers;
         } catch (JSONException e) {
             throw new CoreException(e.toString());
         }
-        HashMap<String, IntentionSet> controllers = new HashMap<>();
-        for (int i = 0; i < json.length(); i++) {
-            IntentionSet controller = new IntentionSet(json.getJSONOption(i));
-            if (controller.hasIntentions()) {
-                controllers.put(controller.getRemoteID(), controller);
-            }
-        }
-        return controllers;
     }
 
     public int getPort() {
         return this.clientSocket.getPort();
     }
 
-    public ScenarioConfig getScenarioBlocking() throws CoreException, IOException {
+    public ScenarioConfig getScenarioBlocking() throws CoreException {
         return new ScenarioConfig(this.getInputBlocking());
     }
 
-    public Snapshot getSnapshotBlocking() throws CoreException, IOException {
+    public Snapshot getSnapshotBlocking() throws CoreException {
         return new Snapshot(this.getInputBlocking());
     }
 
@@ -129,24 +156,40 @@ public class JSONSocket {
         this.out.println(encoding);
     }
 
-    public void send(JSONOption option) {
-        this.out.println(option.toString());
-    }
-
-    public void sendIntentions(ArrayList<IntentionSet> controllers) {
-        JSONArrayBuilder json = JSONBuilder.Array();
-        for (IntentionSet controller : controllers) {
-            json.put(controller.toJSON());
+    public void send(JSONOption option) throws CoreException {
+        try {
+            this.out.println(option.toString());
+        } catch (JSONException e) {
+            throw new CoreException(e.toString());
         }
-        this.send(json.toJSON().toString());
     }
 
-    public void sendScenario(ScenarioConfig scenario) {
-        this.send(scenario.encode());
+    public void sendIntentions(ArrayList<IntentionSet> controllers) throws CoreException {
+        try {
+            JSONArrayBuilder json = JSONBuilder.Array();
+            for (IntentionSet controller : controllers) {
+                json.put(controller.toJSON());
+            }
+            this.send(json.toJSON().toString());
+        } catch (JSONException e) {
+            throw new CoreException(e.toString());
+        }
     }
 
-    public void sendSnapshot(ScenarioConfig scenario) {
-        this.send(scenario.encode());
+    public void sendScenario(ScenarioConfig scenario) throws CoreException {
+        try {
+            this.send(scenario.encode());
+        } catch (JSONException e) {
+            throw new CoreException(e.toString());
+        }
+    }
+
+    public void sendSnapshot(Snapshot snap) throws CoreException {
+        try {
+            this.send(snap.encode());
+        } catch (JSONException e) {
+            throw new CoreException(e.toString());
+        }
     }
 
 }
