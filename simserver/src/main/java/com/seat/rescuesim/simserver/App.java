b4924555204ddae6eb08c8ac5d0118bb405a99d6
Ian Riley <isr413@utulsa.edu>
2022-04-08 11:54:45 -0500

    Updates server App

--- simserver/src/main/java/com/seat/rescuesim/simserver/App.java
+++ simserver/src/main/java/com/seat/rescuesim/simserver/App.java
@@ -1,135 +1,20 @@
 package com.seat.rescuesim.simserver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.HashMap;
-
-import com.seat.rescuesim.common.ScenarioConfig;
-import com.seat.rescuesim.common.Snapshot;
-import com.seat.rescuesim.common.json.JSONArray;
-import com.seat.rescuesim.common.json.JSONException;
-import com.seat.rescuesim.common.json.JSONOption;
-import com.seat.rescuesim.common.remote.RemoteController;
 import com.seat.rescuesim.common.util.ArgsParser;
-import com.seat.rescuesim.common.util.Debugger;
-import com.seat.rescuesim.simserver.sim.SimScenario;
+import com.seat.rescuesim.simserver.core.AppServer;
 
 public class App {
-
-    private static final int DEFAULT_PORT = 8080;
-    private static final String PORT_PARAM = "-p";
-
-    private static String getInputBlocking(BufferedReader in) throws IOException, JSONException {
-        while (true) {
-            if (in.ready()) {
-                String encoding = in.readLine();
-                if (!JSONOption.isJSON(encoding)) {
-                    throw new JSONException(String.format("Cannot decode %s", encoding));
-                }
-                return encoding;
-            }
-        }
-    }
-
-    private static ServerSocket getServerSocketBlocking(ArgsParser args) throws IOException {
-        int port = App.DEFAULT_PORT;
-        if (args.hasParam(App.PORT_PARAM)) {
-            port = args.getInt(App.PORT_PARAM);
-        }
-        ServerSocket server = new ServerSocket(port);
-        Debugger.logger.info(String.format("Listening on port=%d ...", port));
-        return server;
-    }
-
-    private static ScenarioConfig getScenarioConfigBlocking(BufferedReader in) throws IOException, JSONException {
-        Debugger.logger.info("Waiting for scenario config ...");
-        String encoding = App.getInputBlocking(in);
-        ScenarioConfig scenarioConfig = new ScenarioConfig(encoding);
-        Debugger.logger.state(String.format("Received scenario <%s>", scenarioConfig.getScenarioID()));
-        return scenarioConfig;
-    }
-
-    private static HashMap<String, RemoteController> getRemoteIntentionsBlocking(SimScenario scenario,
-            BufferedReader in) throws IOException, JSONException {
-        if (!scenario.hasRemotes()) {
-            Debugger.logger.info(String.format("No remotes for <%s>", scenario.getScenarioID()));
-            return new HashMap<>();
-        }
-        Debugger.logger.info(String.format("Waiting for intentions for remotes %s ...",
-            scenario.getActiveDynamicRemoteIDs().toString()));
-        String encoding = App.getInputBlocking(in);
-        HashMap<String, RemoteController> controllers = new HashMap<>();
-        JSONArray json = JSONOption.String(encoding).someArray();
-        for (int i = 0; i < json.length(); i++) {
-            RemoteController controller = new RemoteController(json.getJSONObject(i));
-            if (controller.hasIntentions()) {
-                controllers.put(controller.getRemoteID(), controller);
-            }
-        }
-        Debugger.logger.state(String.format("Received intentions for remotes %s ...", controllers.keySet().toString()));
-        return controllers;
-    }
-
-    private static void runScenarioSync(ScenarioConfig config, BufferedReader in, PrintWriter out) {
-        Debugger.logger.info(String.format("Running scenario <%s> ...", config.getScenarioID()));
-        SimScenario scenario = new SimScenario(config);
-        Snapshot snap = scenario.getSnapshot();
-        Debugger.logger.info(String.format("Sending initial snap <%s> ...", snap.getHash()));
-        out.println(snap.encode());
-        double time = 0.0;
-        while (time < scenario.getMissionLength()) {
-            try {
-                HashMap<String, RemoteController> controllers = App.getRemoteIntentionsBlocking(scenario, in);
-                Debugger.logger.info(String.format("Updating scenario <%s> for time=%.2f ...", scenario.getScenarioID(),
-                    time + scenario.getStepSize()));
-                if (scenario.getMissionLength() < time + scenario.getStepSize()) {
-                    scenario.update(controllers, scenario.getMissionLength() - time);
-                    time = scenario.getMissionLength();
-                } else {
-                    scenario.update(controllers, scenario.getStepSize());
-                    time += scenario.getStepSize();
-                }
-                Debugger.logger.info(String.format("Sending snap <%s> ...", snap.getHash()));
-                snap = scenario.getSnapshot();
-                out.println(snap.encode());
-                if (scenario.isDone()) {
-                    Debugger.logger.state(String.format("Scenario <%s> is done", scenario.getScenarioID()));
-                    break;
-                }
-            } catch (Exception e) {
-                if (out != null) {
-                    out.println(e.toString());
-                }
-                e.printStackTrace();
-            }
-        }
-    }
+    private static final String PORT_ARG = "-p";
 
     public static void main(String[] args) {
-        ServerSocket server = null;
-        Socket client = null;
-        BufferedReader in = null;
-        PrintWriter out = null;
-        try {
-            Debugger.logger.info("Sim server starting ...");
-            ArgsParser argsParser = new ArgsParser(args);
-            server = App.getServerSocketBlocking(argsParser);
-            client = server.accept();
-            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            out = new PrintWriter(client.getOutputStream(), true);
-            ScenarioConfig scenarioConfig = App.getScenarioConfigBlocking(in);
-            App.runScenarioSync(scenarioConfig, in, out);
-            server.close();
-        } catch (Exception e) {
-            System.err.println(e);
-            if (out != null) {
-                out.println(e.toString());
-            }
+        ArgsParser parser = new ArgsParser(args);
+        AppServer server;
+        if (parser.hasParam(App.PORT_ARG)) {
+            server = new AppServer(parser.getInt(App.PORT_ARG));
+        } else {
+            server = new AppServer();
         }
+        server.run();
     }
 
 }
