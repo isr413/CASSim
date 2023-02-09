5bca48c3eafcb435a548978fc5bb1ec5a1e617d6
latenitecoding <latenitecode@gmail.com>
2023-02-09 09:04:45 -0600

    fixes spacing

--- common/src/main/java/com/seat/sim/common/gui/GUIGridFrame.java
+++ common/src/main/java/com/seat/sim/common/gui/GUIGridFrame.java
@@ -18,196 +18,198 @@ import com.seat.sim.common.scenario.Snapshot;
 
 public class GUIGridFrame extends JFrame {
 
-    protected static final int DEFAULT_PANEL_WIDTH = 1280;
-    protected static final int DEFAULT_PANEL_HEIGHT = 720;
-    protected static final String DEFAULT_TITLE = "App";
+  protected static final int DEFAULT_PANEL_WIDTH = 1280;
+  protected static final int DEFAULT_PANEL_HEIGHT = 720;
+  protected static final String DEFAULT_TITLE = "App";
 
-    private GUIGridPanel panel;
+  private GUIGridPanel panel;
 
-    public GUIGridFrame(Grid grid) {
-        this(grid.getWidth(), grid.getHeight(), grid.getZoneSize());
+  public GUIGridFrame(Grid grid) {
+    this(grid.getWidth(), grid.getHeight(), grid.getZoneSize());
+  }
+
+  public GUIGridFrame(int gridWidth, int gridHeight, int zoneSize) {
+    this(GUIGridFrame.DEFAULT_TITLE, gridWidth, gridHeight, zoneSize);
+  }
+
+  public GUIGridFrame(String title, Grid grid) {
+    this(title, grid.getWidth(), grid.getHeight(), grid.getZoneSize());
+  }
+
+  public GUIGridFrame(String title, int gridWidth, int gridHeight, int zoneSize) {
+    this(title, GUIGridFrame.DEFAULT_PANEL_WIDTH, GUIGridFrame.DEFAULT_PANEL_HEIGHT, gridWidth, gridHeight, zoneSize);
+  }
+
+  public GUIGridFrame(int panelWidth, int panelHeight, Grid grid) {
+    this(panelWidth, panelHeight, grid.getWidth(), grid.getHeight(), grid.getZoneSize());
+  }
+
+  public GUIGridFrame(int panelWidth, int panelHeight, int gridWidth, int gridHeight, int zoneSize) {
+    this(GUIGridFrame.DEFAULT_TITLE, panelWidth, panelHeight, gridWidth, gridHeight, zoneSize);
+  }
+
+  public GUIGridFrame(String title, int panelWidth, int panelHeight, Grid grid) {
+    this(title, panelWidth, panelHeight, grid.getWidth(), grid.getHeight(), grid.getZoneSize());
+  }
+
+  public GUIGridFrame(String title, int panelWidth, int panelHeight, int gridWidth, int gridHeight, int zoneSize) {
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    if (screenSize.getWidth() < panelWidth || screenSize.getHeight() < panelHeight) {
+      panelWidth = (int) screenSize.getWidth();
+      panelHeight = (int) screenSize.getHeight();
+    }
+    this.panel = new GUIGridPanel(panelWidth, panelHeight, gridWidth, gridHeight, zoneSize);
+    add(this.panel);
+    setTitle(title);
+    setSize(panelWidth, panelHeight);
+    setLocationRelativeTo(null);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+
+  public void displaySnap(Snapshot snap) {
+    ArrayList<Point> points = new ArrayList<>();
+    for (RemoteState remoteState : snap.getRemoteStates()) {
+      if (!(remoteState.isEnabled() && remoteState.hasLocation())) {
+        continue;
+      }
+      points.add(Point.Colored(
+        (int) Math.round(remoteState.getLocation().getX()),
+        (int) Math.round(remoteState.getLocation().getY()),
+        remoteState.getTeam())
+      );
+    }
+    this.panel.paintPoints(points);
+  }
+
+  static class GUIGridPanel extends JPanel implements ActionListener {
+
+    protected static final Color DEFAULT_GRID_COLOR = Color.LIGHT_GRAY;
+    protected static final Color DEFAULT_MAP_BOUNDS_COLOR = Color.DARK_GRAY;
+    protected static final int DEFAULT_POINT_SIZE = 4;
+
+    private int gridHeight;
+    private int gridWidth;
+    private int height;
+    private ArrayList<Point> points;
+    private int width;
+    private int zoneSize;
+
+    public GUIGridPanel(int width, int height, int gridWidth, int gridHeight, int zoneSize) {
+      this.width = width;
+      this.height = height;
+      this.gridWidth = gridWidth;
+      this.gridHeight = gridHeight;
+      this.zoneSize = zoneSize;
+      this.points = new ArrayList<>();
     }
 
-    public GUIGridFrame(int gridWidth, int gridHeight, int zoneSize) {
-        this(GUIGridFrame.DEFAULT_TITLE, gridWidth, gridHeight, zoneSize);
+    private void drawDisplay(Graphics g) {
+      Graphics2D g2d = (Graphics2D) g;
+      this.drawGridLines(g2d);
+      this.drawGridBounds(g2d);
+      this.drawPoints(g2d);
     }
 
-    public GUIGridFrame(String title, Grid grid) {
-        this(title, grid.getWidth(), grid.getHeight(), grid.getZoneSize());
+    private void drawPoints(Graphics2D g2d) {
+      for (Point p : this.points) {
+        g2d.setPaint(p.color);
+        g2d.fillOval(
+          this.getGridBoundsX() + p.x - GUIGridPanel.DEFAULT_POINT_SIZE/2,
+          this.getGridBoundsY() + p.y - GUIGridPanel.DEFAULT_POINT_SIZE/2,
+          GUIGridPanel.DEFAULT_POINT_SIZE,
+          GUIGridPanel.DEFAULT_POINT_SIZE
+        );
+      }
     }
 
-    public GUIGridFrame(String title, int gridWidth, int gridHeight, int zoneSize) {
-        this(title, GUIGridFrame.DEFAULT_PANEL_WIDTH, GUIGridFrame.DEFAULT_PANEL_HEIGHT, gridWidth, gridHeight, zoneSize);
+    private void drawGridLines(Graphics2D g2d) {
+      g2d.setPaint(GUIGridPanel.DEFAULT_GRID_COLOR);
+      for (int i = this.getGridBoundsX() + this.zoneSize; i < this.getGridBoundsX() + this.gridWidth;
+          i += this.zoneSize) {
+        g2d.drawLine(i, this.getGridBoundsY(), i, this.getGridBoundsY() + this.gridHeight);
+      }
+      for (int j = this.getGridBoundsY() + this.zoneSize; j < this.getGridBoundsY() + this.gridHeight;
+          j += this.zoneSize) {
+        g2d.drawLine(this.getGridBoundsX(), j, this.getGridBoundsX() + this.gridWidth, j);
+      }
     }
 
-    public GUIGridFrame(int panelWidth, int panelHeight, Grid grid) {
-        this(panelWidth, panelHeight, grid.getWidth(), grid.getHeight(), grid.getZoneSize());
+    private void drawGridBounds(Graphics2D g2d) {
+      g2d.setPaint(GUIGridPanel.DEFAULT_MAP_BOUNDS_COLOR);
+      g2d.drawRect(this.getGridBoundsX(), this.getGridBoundsY(), this.gridWidth, this.gridHeight);
     }
 
-    public GUIGridFrame(int panelWidth, int panelHeight, int gridWidth, int gridHeight, int zoneSize) {
-        this(GUIGridFrame.DEFAULT_TITLE, panelWidth, panelHeight, gridWidth, gridHeight, zoneSize);
+    private int getGridBoundsX() {
+      return this.getCenterX() - (int) Math.round(this.gridWidth / 2.0);
     }
 
-    public GUIGridFrame(String title, int panelWidth, int panelHeight, Grid grid) {
-        this(title, panelWidth, panelHeight, grid.getWidth(), grid.getHeight(), grid.getZoneSize());
+    private int getGridBoundsY() {
+      return this.getCenterY() - (int) Math.round(this.gridHeight / 2.0);
     }
 
-    public GUIGridFrame(String title, int panelWidth, int panelHeight, int gridWidth, int gridHeight, int zoneSize) {
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        if (screenSize.getWidth() < panelWidth || screenSize.getHeight() < panelHeight) {
-            panelWidth = (int) screenSize.getWidth();
-            panelHeight = (int) screenSize.getHeight();
-        }
-        this.panel = new GUIGridPanel(panelWidth, panelHeight, gridWidth, gridHeight, zoneSize);
-        add(this.panel);
-        setTitle(title);
-        setSize(panelWidth, panelHeight);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      repaint();
     }
 
-    public void displaySnap(Snapshot snap) {
-        ArrayList<Point> points = new ArrayList<>();
-        for (RemoteState remoteState : snap.getRemoteStates()) {
-            if (!(remoteState.isEnabled() && remoteState.hasLocation())) continue;
-            points.add(Point.Colored(
-                (int) Math.round(remoteState.getLocation().getX()),
-                (int) Math.round(remoteState.getLocation().getY()),
-                remoteState.getTeam())
-            );
-        }
-        this.panel.paintPoints(points);
+    public int getCenterX() {
+      return (int) Math.round(this.width / 2.0);
     }
 
-    static class GUIGridPanel extends JPanel implements ActionListener {
-
-        protected static final Color DEFAULT_GRID_COLOR = Color.LIGHT_GRAY;
-        protected static final Color DEFAULT_MAP_BOUNDS_COLOR = Color.DARK_GRAY;
-        protected static final int DEFAULT_POINT_SIZE = 4;
-
-        private int gridHeight;
-        private int gridWidth;
-        private int height;
-        private ArrayList<Point> points = new ArrayList<>();
-        private int width;
-        private int zoneSize;
-
-        public GUIGridPanel(int width, int height, int gridWidth, int gridHeight, int zoneSize) {
-            this.width = width;
-            this.height = height;
-            this.gridWidth = gridWidth;
-            this.gridHeight = gridHeight;
-            this.zoneSize = zoneSize;
-        }
-
-        private void drawDisplay(Graphics g) {
-            Graphics2D g2d = (Graphics2D) g;
-            this.drawGridLines(g2d);
-            this.drawGridBounds(g2d);
-            this.drawPoints(g2d);
-        }
-
-        private void drawPoints(Graphics2D g2d) {
-            for (Point p : this.points) {
-                g2d.setPaint(p.color);
-                g2d.fillOval(
-                    this.getGridBoundsX() + p.x - GUIGridPanel.DEFAULT_POINT_SIZE/2,
-                    this.getGridBoundsY() + p.y - GUIGridPanel.DEFAULT_POINT_SIZE/2,
-                    GUIGridPanel.DEFAULT_POINT_SIZE,
-                    GUIGridPanel.DEFAULT_POINT_SIZE
-                );
-            }
-        }
-
-        private void drawGridLines(Graphics2D g2d) {
-            g2d.setPaint(GUIGridPanel.DEFAULT_GRID_COLOR);
-            for (int i = this.getGridBoundsX() + this.zoneSize; i < this.getGridBoundsX() + this.gridWidth;
-                    i += this.zoneSize) {
-                g2d.drawLine(i, this.getGridBoundsY(), i, this.getGridBoundsY() + this.gridHeight);
-            }
-            for (int j = this.getGridBoundsY() + this.zoneSize; j < this.getGridBoundsY() + this.gridHeight;
-                    j += this.zoneSize) {
-                g2d.drawLine(this.getGridBoundsX(), j, this.getGridBoundsX() + this.gridWidth, j);
-            }
-        }
-
-        private void drawGridBounds(Graphics2D g2d) {
-            g2d.setPaint(GUIGridPanel.DEFAULT_MAP_BOUNDS_COLOR);
-            g2d.drawRect(this.getGridBoundsX(), this.getGridBoundsY(), this.gridWidth, this.gridHeight);
-        }
-
-        private int getGridBoundsX() {
-            return this.getCenterX() - (int) Math.round(this.gridWidth / 2.0);
-        }
-
-        private int getGridBoundsY() {
-            return this.getCenterY() - (int) Math.round(this.gridHeight / 2.0);
-        }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            repaint();
-        }
-
-        public int getCenterX() {
-            return (int) Math.round(this.width / 2.0);
-        }
-
-        public int getCenterY() {
-            return (int) Math.round(this.height / 2.0);
-        }
-
-        public int getHeight() {
-            return this.height;
-        }
-
-        public int getWidth() {
-            return this.width;
-        }
-
-        @Override
-        public void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            drawDisplay(g);
-        }
-
-        public void paintPoints(ArrayList<Point> points) {
-            this.points = points;
-            repaint();
-        }
-
+    public int getCenterY() {
+      return (int) Math.round(this.height / 2.0);
     }
 
-    static class Point {
-        public int x;
-        public int y;
-        public Color color;
-
-        public static Point Colored(int x, int y, TeamColor team) {
-            switch (team) {
-                case BLUE: return new Point(x, y, Color.BLUE);
-                case CYAN: return new Point(x, y, Color.CYAN);
-                case GRAY: return new Point(x, y, Color.GRAY);
-                case GREEN: return new Point(x, y, Color.GREEN);
-                case MAGENTA: return new Point(x, y, Color.MAGENTA);
-                case ORANGE: return new Point(x, y, Color.ORANGE);
-                case PINK: return new Point(x, y, Color.PINK);
-                case RED: return new Point(x, y, Color.RED);
-                case YELLOW: return new Point(x, y, Color.YELLOW);
-                default: return new Point(x, y, Color.BLACK);
-            }
-        }
-
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public Point(int x, int y, Color color) {
-            this(x, y);
-            this.color = color;
-        }
-
+    public int getHeight() {
+      return this.height;
     }
 
+    public int getWidth() {
+      return this.width;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+      super.paintComponent(g);
+      drawDisplay(g);
+    }
+
+    public void paintPoints(ArrayList<Point> points) {
+      this.points = points;
+      repaint();
+    }
+
+  }
+
+  static class Point {
+    public int x;
+    public int y;
+    public Color color;
+
+    public static Point Colored(int x, int y, TeamColor team) {
+      switch (team) {
+        case BLUE: return new Point(x, y, Color.BLUE);
+        case CYAN: return new Point(x, y, Color.CYAN);
+        case GRAY: return new Point(x, y, Color.GRAY);
+        case GREEN: return new Point(x, y, Color.GREEN);
+        case MAGENTA: return new Point(x, y, Color.MAGENTA);
+        case ORANGE: return new Point(x, y, Color.ORANGE);
+        case PINK: return new Point(x, y, Color.PINK);
+        case RED: return new Point(x, y, Color.RED);
+        case YELLOW: return new Point(x, y, Color.YELLOW);
+        default: return new Point(x, y, Color.BLACK);
+      }
+    }
+
+    public Point(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    public Point(int x, int y, Color color) {
+      this(x, y);
+      this.color = color;
+    }
+
+  }
 }
--- common/src/main/java/com/seat/sim/common/net/JsonSocket.java
+++ common/src/main/java/com/seat/sim/common/net/JsonSocket.java
@@ -19,186 +19,186 @@ import com.seat.sim.common.scenario.Snapshot;
 
 public class JsonSocket {
 
-    protected static final InetAddress DEFAULT_ADDRESS = InetAddress.getLoopbackAddress();
-    protected static final int DEFAULT_PORT = 8080;
+  protected static final InetAddress DEFAULT_ADDRESS = InetAddress.getLoopbackAddress();
+  protected static final int DEFAULT_PORT = 8080;
 
-    public static JsonSocket Client() throws CommonException {
-        return JsonSocket.Client(JsonSocket.DEFAULT_ADDRESS, JsonSocket.DEFAULT_PORT);
+  public static JsonSocket Client() throws CommonException {
+    return JsonSocket.Client(JsonSocket.DEFAULT_ADDRESS, JsonSocket.DEFAULT_PORT);
+  }
+
+  public static JsonSocket Client(int port) throws CommonException {
+    return JsonSocket.Client(JsonSocket.DEFAULT_ADDRESS, port);
+  }
+
+  public static JsonSocket Client(InetAddress address) throws CommonException {
+    try {
+      return new JsonSocket(new Socket(address, JsonSocket.DEFAULT_PORT));
+    } catch(IOException e) {
+      throw new CommonException(e.toString());
     }
+  }
 
-    public static JsonSocket Client(int port) throws CommonException {
-        return JsonSocket.Client(JsonSocket.DEFAULT_ADDRESS, port);
+  public static JsonSocket Client(String address) throws CommonException {
+    try {
+      return new JsonSocket(new Socket(address, JsonSocket.DEFAULT_PORT));
+    } catch(IOException e) {
+      throw new CommonException(e.toString());
     }
+  }
 
-    public static JsonSocket Client(InetAddress address) throws CommonException {
-        try {
-            return new JsonSocket(new Socket(address, JsonSocket.DEFAULT_PORT));
-        } catch(IOException e) {
-            throw new CommonException(e.toString());
+  public static JsonSocket Client(InetAddress address, int port) throws CommonException {
+    try {
+      return new JsonSocket(new Socket(address, port));
+    } catch(IOException e) {
+      throw new CommonException(e.toString());
+    }
+  }
+
+  public static JsonSocket Client(String address, int port) throws CommonException {
+    try {
+      return new JsonSocket(new Socket(address, port));
+    } catch(IOException e) {
+      throw new CommonException(e.toString());
+    }
+  }
+
+  public static JsonSocket Server() throws CommonException {
+    return JsonSocket.Server(JsonSocket.DEFAULT_PORT);
+  }
+
+  public static JsonSocket Server(int port) throws CommonException {
+    try {
+      return new JsonSocket(new ServerSocket(port));
+    } catch(IOException e) {
+      throw new CommonException(e.toString());
+    }
+  }
+
+  private ServerSocket serverSocket;
+  private Socket clientSocket;
+  private BufferedReader in;
+  private PrintWriter out;
+
+  private JsonSocket(ServerSocket socket) throws CommonException {
+    try {
+      this.serverSocket = socket;
+      this.clientSocket = this.serverSocket.accept();
+      this.in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+      this.out = new PrintWriter(this.clientSocket.getOutputStream(), true);
+    } catch (IOException e) {
+      throw new CommonException(e.toString());
+    }
+  }
+
+  private JsonSocket(Socket socket) throws CommonException {
+    try {
+      this.serverSocket = null;
+      this.clientSocket = socket;
+      this.in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+      this.out = new PrintWriter(this.clientSocket.getOutputStream(), true);
+    } catch (IOException e) {
+      throw new CommonException(e.toString());
+    }
+  }
+
+  public void close() throws CommonException {
+    try {
+      this.out.close();
+      this.in.close();
+      this.clientSocket.close();
+      if (this.serverSocket != null) {
+        this.serverSocket.close();
+      }
+    } catch (IOException e) {
+      throw new CommonException(e.toString());
+    }
+  }
+
+  public String getAddress() {
+    return (this.serverSocket != null) ?
+      this.serverSocket.getInetAddress().toString() :
+      this.clientSocket.getInetAddress().toString();
+  }
+
+  public Json getInputBlocking() throws CommonException {
+    try {
+      while (true) {
+        if (this.in.ready()) {
+          String encoding = in.readLine();
+          Json json = Json.of(encoding);
+          return json;
         }
+      }
+    } catch(IOException e) {
+      throw new CommonException(e.toString());
     }
+  }
 
-    public static JsonSocket Client(String address) throws CommonException {
-        try {
-            return new JsonSocket(new Socket(address, JsonSocket.DEFAULT_PORT));
-        } catch(IOException e) {
-            throw new CommonException(e.toString());
+  public Map<String, IntentionSet> getIntentionsBlocking() throws CommonException {
+    try {
+      JsonArray json = this.getInputBlocking().getJsonArray();
+      HashMap<String, IntentionSet> controllers = new HashMap<>();
+      for (int i = 0; i < json.length(); i++) {
+        IntentionSet controller = new IntentionSet(json.getJson(i));
+        if (controller.hasIntentions()) {
+          controllers.put(controller.getRemoteID(), controller);
         }
+      }
+      return controllers;
+    } catch (JsonException e) {
+      throw new CommonException(e.toString());
     }
+  }
 
-    public static JsonSocket Client(InetAddress address, int port) throws CommonException {
-        try {
-            return new JsonSocket(new Socket(address, port));
-        } catch(IOException e) {
-            throw new CommonException(e.toString());
-        }
+  public int getPort() {
+    return this.clientSocket.getPort();
+  }
+
+  public ScenarioConfig getScenarioConfigBlocking() throws CommonException {
+    return new ScenarioConfig(this.getInputBlocking());
+  }
+
+  public Snapshot getSnapshotBlocking() throws CommonException {
+    return new Snapshot(this.getInputBlocking());
+  }
+
+  public void send(String encoding) {
+    this.out.println(encoding);
+  }
+
+  public void send(Json json) throws CommonException {
+    try {
+      this.out.println(json.toString());
+    } catch (JsonException e) {
+      throw new CommonException(e.toString());
     }
+  }
 
-    public static JsonSocket Client(String address, int port) throws CommonException {
-        try {
-            return new JsonSocket(new Socket(address, port));
-        } catch(IOException e) {
-            throw new CommonException(e.toString());
-        }
+  public void sendIntentions(Collection<IntentionSet> intentions) throws CommonException {
+    try {
+      JsonArrayBuilder json = JsonBuilder.Array();
+      for (IntentionSet intention : intentions) {
+        json.put(intention.toJson());
+      }
+      this.send(json.toJson().toString());
+    } catch (JsonException e) {
+      throw new CommonException(e.toString());
     }
+  }
 
-    public static JsonSocket Server() throws CommonException {
-        return JsonSocket.Server(JsonSocket.DEFAULT_PORT);
+  public void sendScenarioConfig(ScenarioConfig scenario) throws CommonException {
+    try {
+      this.send(scenario.encode());
+    } catch (JsonException e) {
+      throw new CommonException(e.toString());
     }
+  }
 
-    public static JsonSocket Server(int port) throws CommonException {
-        try {
-            return new JsonSocket(new ServerSocket(port));
-        } catch(IOException e) {
-            throw new CommonException(e.toString());
-        }
+  public void sendSnapshot(Snapshot snap) throws CommonException {
+    try {
+      this.send(snap.encode());
+    } catch (JsonException e) {
+      throw new CommonException(e.toString());
     }
-
-    private ServerSocket serverSocket;
-    private Socket clientSocket;
-    private BufferedReader in;
-    private PrintWriter out;
-
-    private JsonSocket(ServerSocket socket) throws CommonException {
-        try {
-            this.serverSocket = socket;
-            this.clientSocket = this.serverSocket.accept();
-            this.in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
-            this.out = new PrintWriter(this.clientSocket.getOutputStream(), true);
-        } catch (IOException e) {
-            throw new CommonException(e.toString());
-        }
-    }
-
-    private JsonSocket(Socket socket) throws CommonException {
-        try {
-            this.serverSocket = null;
-            this.clientSocket = socket;
-            this.in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
-            this.out = new PrintWriter(this.clientSocket.getOutputStream(), true);
-        } catch (IOException e) {
-            throw new CommonException(e.toString());
-        }
-    }
-
-    public void close() throws CommonException {
-        try {
-            this.out.close();
-            this.in.close();
-            this.clientSocket.close();
-            if (this.serverSocket != null) {
-                this.serverSocket.close();
-            }
-        } catch (IOException e) {
-            throw new CommonException(e.toString());
-        }
-    }
-
-    public String getAddress() {
-        return (this.serverSocket != null) ?
-            this.serverSocket.getInetAddress().toString() :
-            this.clientSocket.getInetAddress().toString();
-    }
-
-    public Json getInputBlocking() throws CommonException {
-        try {
-            while (true) {
-                if (this.in.ready()) {
-                    String encoding = in.readLine();
-                    Json json = Json.of(encoding);
-                    return json;
-                }
-            }
-        } catch(IOException e) {
-            throw new CommonException(e.toString());
-        }
-    }
-
-    public Map<String, IntentionSet> getIntentionsBlocking() throws CommonException {
-        try {
-            JsonArray json = this.getInputBlocking().getJsonArray();
-            HashMap<String, IntentionSet> controllers = new HashMap<>();
-            for (int i = 0; i < json.length(); i++) {
-                IntentionSet controller = new IntentionSet(json.getJson(i));
-                if (controller.hasIntentions()) {
-                    controllers.put(controller.getRemoteID(), controller);
-                }
-            }
-            return controllers;
-        } catch (JsonException e) {
-            throw new CommonException(e.toString());
-        }
-    }
-
-    public int getPort() {
-        return this.clientSocket.getPort();
-    }
-
-    public ScenarioConfig getScenarioConfigBlocking() throws CommonException {
-        return new ScenarioConfig(this.getInputBlocking());
-    }
-
-    public Snapshot getSnapshotBlocking() throws CommonException {
-        return new Snapshot(this.getInputBlocking());
-    }
-
-    public void send(String encoding) {
-        this.out.println(encoding);
-    }
-
-    public void send(Json json) throws CommonException {
-        try {
-            this.out.println(json.toString());
-        } catch (JsonException e) {
-            throw new CommonException(e.toString());
-        }
-    }
-
-    public void sendIntentions(Collection<IntentionSet> intentions) throws CommonException {
-        try {
-            JsonArrayBuilder json = JsonBuilder.Array();
-            for (IntentionSet intention : intentions) {
-                json.put(intention.toJson());
-            }
-            this.send(json.toJson().toString());
-        } catch (JsonException e) {
-            throw new CommonException(e.toString());
-        }
-    }
-
-    public void sendScenarioConfig(ScenarioConfig scenario) throws CommonException {
-        try {
-            this.send(scenario.encode());
-        } catch (JsonException e) {
-            throw new CommonException(e.toString());
-        }
-    }
-
-    public void sendSnapshot(Snapshot snap) throws CommonException {
-        try {
-            this.send(snap.encode());
-        } catch (JsonException e) {
-            throw new CommonException(e.toString());
-        }
-    }
+  }
 }
