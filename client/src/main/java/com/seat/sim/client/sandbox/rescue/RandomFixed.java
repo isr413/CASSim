84f3c29e3ebce14ef366698b49141370ea460846
latenitecoding <latenitecode@gmail.com>
2023-04-11 14:43:07 -0500

    Updates scenarios

---
+++ client/src/main/java/com/seat/sim/client/sandbox/rescue/RandomFixed.java
@@ -0,0 +1,61 @@
+package com.seat.sim.client.sandbox.rescue;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import com.seat.sim.client.sandbox.rescue.util.RescueScenario;
+import com.seat.sim.client.sandbox.rescue.util.TaskManager;
+import com.seat.sim.common.math.Zone;
+import com.seat.sim.common.remote.RemoteState;
+import com.seat.sim.common.scenario.Snapshot;
+import com.seat.sim.common.util.ArgsParser;
+import com.seat.sim.common.util.Range;
+
+public class RandomFixed extends RescueScenario {
+ 
+  public RandomFixed(ArgsParser args, int threadID, long seed) throws IOException {
+    super("RandomFixed", 1, 32, 1024, 6, new Range(0., .1, 1., true), 0.6, 0.6, 12, threadID, 4, seed);
+  }
+
+  public Optional<TaskManager> getTaskManager() {
+    return Optional.of(new RandomFixedTaskManager(this));
+  }
+
+  private static class RandomFixedTaskManager implements TaskManager {
+    
+    private RescueScenario scenario;
+    private List<Zone> tasks;
+
+    public RandomFixedTaskManager(RescueScenario scenario) {
+      this.scenario = scenario;
+    }
+   
+    public void addTask(Zone task) {
+      this.tasks.add(task);
+    }
+
+    public Optional<Zone> getNextTask(Snapshot snap, RemoteState state) {
+      if (state.getLocation().near(RescueScenario.GRID_CENTER)) {
+        return (this.hasNextTask()) ? Optional.of(this.tasks.remove(0)) : Optional.empty();
+      }
+      return Optional.empty();
+    }
+
+    public boolean hasNextTask() {
+      return !this.tasks.isEmpty();
+    }
+
+    public void init() {
+      this.tasks = scenario.getZones().collect(Collectors.toCollection(LinkedList::new));
+      Collections.shuffle(this.tasks, scenario.getRng().unwrap());
+    }
+
+    public void reset() {
+      this.init();
+    }
+  }
+}
---
+++ client/src/main/java/com/seat/sim/client/sandbox/rescue/RandomTasks.java
@@ -0,0 +1,47 @@
+package com.seat.sim.client.sandbox.rescue;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import com.seat.sim.client.sandbox.rescue.util.RescueScenario;
+import com.seat.sim.client.sandbox.rescue.util.TaskManager;
+import com.seat.sim.common.math.Grid;
+import com.seat.sim.common.math.Zone;
+import com.seat.sim.common.remote.RemoteState;
+import com.seat.sim.common.scenario.Snapshot;
+import com.seat.sim.common.util.ArgsParser;
+import com.seat.sim.common.util.Range;
+
+public class RandomTasks extends RescueScenario {
+ 
+  public RandomTasks(ArgsParser args, int threadID, long seed) throws IOException {
+    super("RandomTasks", 1, 32, 1024, 6, new Range(0., .1, 1., true), 0.6, 0.6, 12, threadID, 4, seed);
+  }
+
+  public Optional<TaskManager> getTaskManager() {
+    return Optional.of(new RandomTaskManager(this));
+  }
+
+  private static class RandomTaskManager implements TaskManager {
+    
+    private RescueScenario scenario;
+
+    public RandomTaskManager(RescueScenario scenario) {
+      this.scenario = scenario;
+    }
+
+    public void addTask(Zone task) {}
+
+    public Optional<Zone> getNextTask(Snapshot snap, RemoteState state) {
+      Grid grid = this.scenario.getGrid().get();
+      return Optional.of(grid.getZoneAtLocation(
+          this.scenario.getRng().getRandomPoint(grid.getWidth()),
+          this.scenario.getRng().getRandomPoint(grid.getHeight())
+        ));
+    }
+
+    public boolean hasNextTask() {
+      return true;
+    }
+  }
+}
