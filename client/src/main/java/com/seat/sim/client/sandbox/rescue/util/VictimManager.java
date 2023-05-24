2beb782aa9d6718342ea63a5182f90ded5732342
latenitecoding <latenitecode@gmail.com>
2023-05-24 13:03:52 -0500

    updates examples

--- client/src/main/java/com/seat/sim/client/sandbox/rescue/util/VictimManager.java
+++ client/src/main/java/com/seat/sim/client/sandbox/rescue/util/VictimManager.java
@@ -6,7 +6,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Stream;
 
 import com.seat.sim.common.math.Grid;
 import com.seat.sim.common.math.Vector;
