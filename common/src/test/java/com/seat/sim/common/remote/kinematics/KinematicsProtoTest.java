021aff2f2c7a40ab1f952fd5ce30a1def9f31862
latenitecoding <latenitecode@gmail.com>
2023-06-22 11:16:58 -0500

    adds tests for remote fuel usage

--- common/src/test/java/com/seat/sim/common/remote/kinematics/KinematicsProtoTest.java
+++ common/src/test/java/com/seat/sim/common/remote/kinematics/KinematicsProtoTest.java
@@ -1,6 +1,7 @@
 package com.seat.sim.common.remote.kinematics;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.is;
 
 import org.junit.Test;
@@ -62,17 +63,84 @@ public class KinematicsProtoTest {
   }
 
   @Test
+  public void hasFuelProto() {
+    assertThat(KinematicsProtoTest.MockKinematicsProto().hasFuelProto(), is(true));
+    assertThat(KinematicsProtoTest.MockKinematicsProto(true).hasFuelProto(), is(false));
+  }
+
+  @Test
+  public void hasLocation() {
+    assertThat(KinematicsProtoTest.MockKinematicsProto().hasLocation(), is(true));
+    assertThat(new KinematicsProto().hasLocation(), is(false));
+  }
+
+  @Test
+  public void hasMotionProto() {
+    assertThat(KinematicsProtoTest.MockKinematicsProto().hasMotionProto(), is(true));
+    assertThat(KinematicsProtoTest.MockKinematicsProto(true).hasMotionProto(), is(false));
+  }
+
+  @Test
+  public void isEnabled() {
+    assertThat(KinematicsProtoTest.MockKinematicsProto().isEnabled(), is(true));
+    assertThat(KinematicsProtoTest.MockKinematicsProto(true).isEnabled(), is(true));
+    assertThat(
+        new KinematicsProto(
+            Vector.ZERO,
+            new FuelProto(0., Vector.ZERO),
+            MotionProtoTest.MockMotionProto()
+          )
+          .isEnabled(),
+        is(false)
+      );
+  }
+
+  @Test
   public void isMobile() {
     assertThat(KinematicsProtoTest.MockKinematicsProto().isMobile(), is(true));
     assertThat(KinematicsProtoTest.MockKinematicsProto(true).isMobile(), is(false));
     assertThat(
         new KinematicsProto(
-            new Vector(1., 1., 1.),
+            Vector.ZERO,
             FuelProtoTest.MockFuelProto(),
             new MotionProto(Vector.ZERO, 0., 0.)
           )
           .isMobile(),
         is(false)
       );
+  }
+
+  @Test
+  public void remoteFuelUsage() {
+    assertThat(KinematicsProtoTest.MockKinematicsProto(true).getRemoteFuelUsage(), closeTo(0., Vector.PRECISION));
+    assertThat(KinematicsProtoTest.MockKinematicsProto(true).getRemoteFuelUsage(1.), closeTo(0., Vector.PRECISION));
+    assertThat(
+        KinematicsProtoTest.MockKinematicsProto(true).getRemoteFuelUsage(new Vector(1., 1., 1.)),
+        closeTo(0., Vector.PRECISION)
+      );
+    KinematicsProto proto = new KinematicsProto(
+        Vector.ZERO,
+        new FuelProto(1., 1., new Vector(1., 0., 0.)),
+        MotionProtoTest.MockMotionProto()
+      );
+    assertThat(proto.getRemoteFuelUsage(), closeTo(1., Vector.PRECISION));
+    assertThat(proto.getRemoteFuelUsage(2.), closeTo(1., Vector.PRECISION));
+    assertThat(proto.getRemoteFuelUsage(new Vector(2., 2., 2.)), closeTo(1., Vector.PRECISION));
+    proto = new KinematicsProto(
+        Vector.ZERO,
+        new FuelProto(1., 1., new Vector(1., 1., 0.)),
+        MotionProtoTest.MockMotionProto()
+      );
+    assertThat(proto.getRemoteFuelUsage(), closeTo(1., Vector.PRECISION));
+    assertThat(proto.getRemoteFuelUsage(2.), closeTo(3., Vector.PRECISION));
+    assertThat(proto.getRemoteFuelUsage(new Vector(3., 4., 0.)), closeTo(6., Vector.PRECISION));
+    proto = new KinematicsProto(
+        Vector.ZERO,
+        new FuelProto(1., 1., new Vector(1., 1., 1.)),
+        MotionProtoTest.MockMotionProto()
+      );
+    assertThat(proto.getRemoteFuelUsage(), closeTo(1., Vector.PRECISION));
+    assertThat(proto.getRemoteFuelUsage(2.), closeTo(3., Vector.PRECISION));
+    assertThat(proto.getRemoteFuelUsage(new Vector(3., 4., 5.)), closeTo(11., Vector.PRECISION));
   }
 }
