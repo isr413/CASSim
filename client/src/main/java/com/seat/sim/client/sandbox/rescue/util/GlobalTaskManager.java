728ec2e7950ee2f42114f17a70c535eacfe3c540
latenitecoding <latenitecode@gmail.com>
2024-02-15 07:26:23 -0600

    updates global task and quad task managers

--- client/src/main/java/com/seat/sim/client/sandbox/rescue/util/GlobalTaskManager.java
+++ client/src/main/java/com/seat/sim/client/sandbox/rescue/util/GlobalTaskManager.java
@@ -1,128 +1,49 @@
 package com.seat.sim.client.sandbox.rescue.util;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
-import java.util.OptionalInt;
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
 
 import com.seat.sim.client.sandbox.rescue.remote.RescueScenario;
-import com.seat.sim.common.math.Grid;
 import com.seat.sim.common.math.Vector;
 import com.seat.sim.common.math.Zone;
 import com.seat.sim.common.remote.RemoteState;
 import com.seat.sim.common.scenario.Snapshot;
 
-public class GlobalTaskManager implements TaskManager {
-
-  private HashMap<Integer, Integer> defer;
-  private RescueScenario scenario;
-  private boolean[][] zones;
-  private int zonesRemaining;
+public class GlobalTaskManager extends HeatmapTaskManager {
 
   public GlobalTaskManager(RescueScenario scenario) {
-    this.scenario = scenario;
+    super(scenario, true, true, true);
   }
-
-  private void assignZone(Zone zone) {
-    int key = this.getZoneKey(zone);
-    if (!this.defer.containsKey(key)) {
-      this.defer.put(key, 0);
-    }
-    this.defer.put(key, this.defer.get(key) + 1);
-    if (this.hasAssignedZone(zone)) {
-      return;
-    }
-    double x = zone.getLocation().getX(), y = zone.getLocation().getY();
-    int size = zone.getSize();
-    this.zones[(int) (y / size)][(int) (x / size)] = true;
-    this.zonesRemaining--;
-  }
-
-  private int getColls(Zone zone) {
-    int key = this.getZoneKey(zone);
-    return (this.defer.containsKey(key)) ? this.defer.get(key) : 0;
-  }
-
-  private int getZoneKey(Zone zone) {
-    int gridSize = this.scenario.getGrid().get().getWidthInZones(), size = zone.getSize();
-    double x = zone.getLocation().getX(), y = zone.getLocation().getY();
-    return ((int) (y / size)) * gridSize + ((int) (x / size));
-  }
-
-  private boolean hasAssignedZone(Zone zone) {
-    double x = zone.getLocation().getX(), y = zone.getLocation().getY();
-    int size = zone.getSize();
-    return this.zones[(int) (y / size)][(int) (x / size)];
-  }
-
-  public void addTask(Zone task) {}
-
-  public void close() {}
 
   public Optional<Zone> getNextTask(Snapshot snap, RemoteState state) {
-    Zone zone = this.scenario.getGrid().get().getZoneAtLocation(state.getLocation());
-    List<Zone> neighborhood = this.scenario.getGrid().get().getNeighborhood(zone, false);
-    Collections.shuffle(neighborhood, this.scenario.getRng().unwrap());
-    if (neighborhood.isEmpty()) {
+    OptionalDouble maxWeight = Arrays.stream(this.zones)
+        .flatMapToDouble((row) -> Arrays.stream(row))
+        .max();
+    if (maxWeight.isEmpty()) {
       return Optional.empty();
     }
-    List<Zone> choices = neighborhood
-      .stream()
-      .filter(neighbor -> !this.hasAssignedZone(neighbor))
-      .toList();
+    List<Zone> choices = IntStream.range(0, this.zones.length)
+        .boxed()
+        .flatMap((i) -> {
+          return IntStream.range(0, this.zones[i].length)
+              .mapToObj((j) -> this.scenario.getGrid().get().getZoneAtLocation(i, j));
+        })
+        .filter((zone) -> !this.defer.containsKey(this.getZoneKey(zone)))
+        .filter((zone) -> Vector.near(this.getZoneWeight(zone), maxWeight.getAsDouble()))
+        .toList();
     if (choices.isEmpty()) {
-      OptionalInt minColls = neighborhood
-        .stream()
-        .mapToInt(neighbor -> this.getColls(neighbor))
-        .min();
-      if (minColls.isPresent()) {
-        choices = neighborhood
-          .stream()
-          .filter(neighbor -> this.getColls(neighbor) == minColls.getAsInt())
-          .toList();
-      } else {
-        choices = neighborhood;
-      }
-      choices = new ArrayList<>(choices);
-      Collections.sort(choices, new Comparator<Zone>() {
-        @Override
-        public int compare(Zone z1, Zone z2) {
-          return -Double.compare(
-              Vector.dist(RescueScenario.GRID_CENTER, z1.getLocation()),
-              Vector.dist(RescueScenario.GRID_CENTER, z2.getLocation())
-            );
-        }
-      });
-      Zone choice = choices.get(0);
-      this.assignZone(choice);
-      return Optional.of(choice);
+      return Optional.empty();
     }
-    Zone choice = choices.get(this.scenario.getRng().getRandomNumber(choices.size()));
-    this.assignZone(choice);
+    choices = new ArrayList<>(choices);
+    Collections.shuffle(choices, this.scenario.getRng().unwrap());
+    Zone choice = choices.get(0);
+    this.deferZone(choice);
     return Optional.of(choice);
-  }
-
-  public boolean hasNextTask() {
-    return this.zonesRemaining > 0;
-  }
-
-  public void init() {
-    Grid grid = this.scenario.getGrid().get();
-    this.zones = new boolean[grid.getHeightInZones()][grid.getWidthInZones()];
-    this.zonesRemaining = grid.getHeightInZones() * grid.getWidthInZones();
-    this.defer = new HashMap<>();
-    this.assignZone(grid.getZoneAtLocation(RescueScenario.GRID_CENTER));
-  }
-
-  public void reset() {
-    this.init();
-  }
-
-  public void update(Snapshot snap) {
-    this.defer.clear();
   }
 }
 
--- client/src/main/java/com/seat/sim/client/sandbox/rescue/util/QuadTaskManager.java
+++ client/src/main/java/com/seat/sim/client/sandbox/rescue/util/QuadTaskManager.java
@@ -13,22 +13,17 @@ import com.seat.sim.common.math.Zone;
 import com.seat.sim.common.remote.RemoteState;
 import com.seat.sim.common.scenario.Snapshot;
 
-public class QuadTaskManager implements TaskManager {
+public class QuadTaskManager extends StochasticHeatmap {
 
   private List<Zone> quadZones;
-  private RescueScenario scenario;
-  private HeatmapTaskManager smgWalk;
 
   public QuadTaskManager(RescueScenario scenario) {
-    this.scenario = scenario;
-    this.smgWalk = new StochasticHeatmap(scenario);
+    super(scenario, true, true, true);
     this.quadZones = List.of();
   }
 
   public QuadTaskManager(RescueScenario scenario, boolean useDefer, boolean useMovement, boolean useMax) {
-    this.scenario = scenario;
-    this.smgWalk = new StochasticHeatmap(scenario, useDefer, useMovement, useMax);
-    this.quadZones = List.of();
+    super(scenario, useDefer, useMovement, useMax);
   }
 
   private List<Zone> getQuadZones() {
@@ -70,37 +65,29 @@ public class QuadTaskManager implements TaskManager {
     return !this.quadZones.isEmpty();
   }
 
-  public void addTask(Zone task) {
-    this.smgWalk.addTask(task);
-  }
-
-  public void close() {
-    this.smgWalk.close();
-  }
-
+  @Override
   public Optional<Zone> getNextTask(Snapshot snap, RemoteState state) {
     if (this.hasQuadZones()) {
       return Optional.of(this.quadZones.remove(this.quadZones.size() - 1));
     }
 
-    return this.smgWalk.getNextTask(snap, state);
+    return super.getNextTask(snap, state);
   }
 
+  @Override
   public boolean hasNextTask() {
-    return this.hasQuadZones() || this.smgWalk.hasNextTask();
+    return this.hasQuadZones() || super.hasNextTask();
   }
 
+  @Override
   public void init() {
-    this.smgWalk.init();
+    super.init();
     this.quadZones = this.getQuadZones();
   }
 
+  @Override
   public void reset() {
-    this.smgWalk.reset();
+    super.reset();
     this.quadZones = this.getQuadZones();
-  }
-
-  public void update(Snapshot snap) {
-    this.smgWalk.update(snap);
   }
 }
